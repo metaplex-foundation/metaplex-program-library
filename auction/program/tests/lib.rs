56f2188429285eaffdf5ac1c136d7afd859b4938
Thorsten Lorenz <thlorenz@gmx.de>
2021-11-29 15:45:30 -0700

    test: fix program names used

--- auction/program/tests/lib.rs
+++ auction/program/tests/lib.rs
@@ -49,11 +49,8 @@ async fn setup_auction(
 ) {
     // Create a program to attach accounts to.
     let program_id = Pubkey::new_unique();
-    let mut program_test = ProgramTest::new(
-        "metaplex_auction",
-        program_id,
-        processor!(process_instruction),
-    );
+    let mut program_test =
+        ProgramTest::new("mpl_auction", program_id, processor!(process_instruction));
 
     // Start executing test.
     let (mut banks_client, payer, recent_blockhash) = program_test.start().await;
