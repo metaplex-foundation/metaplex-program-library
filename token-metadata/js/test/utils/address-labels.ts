f4dc6533ea81a6eebb46347802edd0ba7cdf1199
Thorsten Lorenz <thlorenz@gmx.de>
2021-12-10 15:24:00 -0700

    chore: simplfied address labels wrapper

--- token-metadata/js/test/utils/address-labels.ts
+++ token-metadata/js/test/utils/address-labels.ts
@@ -1,15 +1,9 @@
-import { AddressLabels, KeyLike } from '@metaplex-foundation/amman';
+import { AddressLabels } from '@metaplex-foundation/amman';
 import { logDebug } from '.';
 
 const persistLabelsPath = process.env.ADDRESS_LABEL_PATH;
 const knownLabels = { ['metaqbxxUerdq28cj1RbAWkYQm3ybzjb6a8bt518x1s']: 'mpl-token-metadata' };
 
-export const addressLabels = new AddressLabels(knownLabels, logDebug, persistLabelsPath);
-
-export function addLabel(label: string, key: KeyLike) {
-  addressLabels.addLabel(label, key);
-}
-
-export function isKeyOf(key: KeyLike) {
-  return addressLabels.isKeyOf(key);
-}
+const addressLabels = new AddressLabels(knownLabels, logDebug, persistLabelsPath);
+export const addLabel = addressLabels.addLabel;
+export const isKeyOf = addressLabels.isKeyOf;
