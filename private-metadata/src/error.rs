c91f2d225ef9767ed2cba9b01e5be7ed6a5b313d
Lawrence Wu <larry.wu@solana.com>
2021-12-16 12:14:01 -0600

    Pull encryption primitives locally to prep for modifications

--- private-metadata/src/error.rs
+++ private-metadata/src/error.rs
@@ -50,3 +50,23 @@ impl<T> DecodeError<T> for PrivateMetadataError {
         "Private Metadata Error"
     }
 }
+
+
+// TODO: use spl-zk-token-sdk
+#[derive(Error, Clone, Debug, Eq, PartialEq)]
+pub enum ProofError {
+    #[error("proof failed to verify")]
+    VerificationError,
+    #[error("malformed proof")]
+    FormatError,
+    #[error("number of blinding factors do not match the number of values")]
+    WrongNumBlindingFactors,
+    #[error("attempted to create a proof with bitsize other than \\(8\\), \\(16\\), \\(32\\), or \\(64\\)")]
+    InvalidBitsize,
+    #[error("insufficient generators for the proof")]
+    InvalidGeneratorsLength,
+    #[error(
+        "`zk_token_elgamal::pod::ElGamalCiphertext` contains invalid ElGamalCiphertext ciphertext"
+    )]
+    InconsistentCTData,
+}
