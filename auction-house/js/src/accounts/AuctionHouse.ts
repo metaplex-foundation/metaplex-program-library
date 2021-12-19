1f16a395b99b41fd7d4f39aaf31098175bcc9817
Thorsten Lorenz <thlorenz@gmx.de>
2021-12-19 17:15:31 -0500

    account: started building out account wrapper and added de/serialization tests

--- auction-house/js/src/accounts/AuctionHouse.ts
+++ auction-house/js/src/accounts/AuctionHouse.ts
@@ -1,14 +1,42 @@
 // This will wrap the AuctionHouse account to include various checks
 // The generated account mainly serves to create/serialize/deserialize
 
-import { AccountInfo, PublicKey } from '@solana/web3.js';
-import { AuctionHouseAccountData } from '../generated/accounts';
+import { strict as assert } from 'assert';
+import { AccountInfo, Commitment, Connection, PublicKey } from '@solana/web3.js';
+import { AuctionHouseAccountData, AuctionHouseAccountDataArgs } from '../generated/accounts';
 
 export class AuctionHouseAccount {
-  readonly data: AuctionHouseAccountData;
-  constructor(_pubkey: PublicKey, info: AccountInfo<Buffer>) {
-    // TODO(thlorenz): assert data compatibility
+  constructor(readonly pubkey: PublicKey, readonly data: AuctionHouseAccountData) {}
 
-    [this.data] = AuctionHouseAccountData.fromAccountInfo(info);
+  static fromAccountInfo(pubkey: PublicKey, info: AccountInfo<Buffer>) {
+    assert(
+      AuctionHouseAccount.isCompatible(info.data),
+      `Data of AccountInfo ${info.data} should be of an AuctionHouseAccount`,
+    );
+    const [data] = AuctionHouseAccountData.fromAccountInfo(info);
+    return new AuctionHouseAccount(pubkey, data);
+  }
+
+  static fromAccountArgs(pubkey: PublicKey, args: AuctionHouseAccountDataArgs) {
+    const data = AuctionHouseAccountData.fromArgs(args);
+    return new AuctionHouseAccount(pubkey, data);
+  }
+
+  static isCompatible(buf: Buffer) {
+    return AuctionHouseAccountData.isCompatible(buf);
+  }
+
+  static async getMinimumBalanceForRentExemption(
+    connection: Connection,
+    commitment?: Commitment,
+  ): Promise<number> {
+    return AuctionHouseAccountData.getMinimumBalanceForRentExemption(connection, commitment);
+  }
+
+  get pretty() {
+    return {
+      pubkey: this.pubkey.toBase58(),
+      data: this.data.pretty,
+    };
   }
 }
