0f24d461399daefe80ed3f816d4063440c356d09
Vadim <grozinok@gmail.com>
2021-12-29 11:06:19 +0200

    feat: init membership-token dir

---
+++ membership-token/src/lib.rs
@@ -0,0 +1,14 @@
+use anchor_lang::prelude::*;
+
+declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+
+#[program]
+pub mod membership_token {
+    use super::*;
+    pub fn initialize(ctx: Context<Initialize>) -> ProgramResult {
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+pub struct Initialize {}
