74c79ee29f5ec1ad5e92a0a3bc1349c2a1dcf8e6
Lawrence Wu <larry.wu@solana.com>
2022-01-09 11:33:48 -0600

    Spike tests up to private metadata creation

--- private-metadata/program/tests/transfer.rs
+++ private-metadata/program/tests/transfer.rs
@@ -1,36 +1,32 @@
 use {
-    solana_program::pubkey::Pubkey,
+    private_metadata::{
+        encryption::elgamal::{
+            ElGamalKeypair,
+            CipherKey,
+        },
+    },
+    rand_core::OsRng,
     solana_program_test::*,
     solana_sdk::{
+        program_pack::Pack,
+        pubkey::Pubkey,
         signer::keypair::Keypair,
         signature::Signer,
         system_instruction,
         transaction::Transaction,
     },
-    private_metadata::{
-        instruction,
-    },
 };
 
-#[tokio::test]
-async fn test_transfer() {
-    let mut pc = ProgramTest::default();
-
-    pc.prefer_bpf(true);
-
-    pc.add_program("private_metadata", private_metadata::id(), None);
-    pc.add_program("mpl_token_metadata", mpl_token_metadata::id(), None);
-    pc.add_program("spl_token", spl_token::id(), None);
-
-    // pc.set_bpf_compute_max_units(350_000);
-
-    let (mut banks_client, payer, recent_blockhash) = pc.start().await;
-
-    let rent = banks_client.get_rent().await;
-    let rent = rent.unwrap();
-
-    let mint = Keypair::new();
-    let (public_metadata_key, _public_metadat_bump) = Pubkey::find_program_address(
+async fn nft_setup_transaction(
+    payer: &dyn Signer,
+    mint: &dyn Signer,
+    token_account: &dyn Signer,
+    recent_blockhash: &solana_sdk::hash::Hash,
+    rent: &solana_sdk::sysvar::rent::Rent,
+    elgamal_kp: &ElGamalKeypair,
+    cipher_key: &CipherKey,
+) -> Result<Transaction, Box<dyn std::error::Error>> {
+    let (public_metadata_key, _public_metadata_bump) = Pubkey::find_program_address(
         &[
             mpl_token_metadata::state::PREFIX.as_bytes(),
             mpl_token_metadata::id().as_ref(),
@@ -39,23 +35,45 @@ async fn test_transfer() {
         &mpl_token_metadata::id(),
     );
 
-    let token_account = Keypair::new(); // not ATA...
-
-    let mut transaction = Transaction::new_with_payer(
+    let (public_edition_key, _public_edition_bump) = Pubkey::find_program_address(
         &[
+            mpl_token_metadata::state::PREFIX.as_bytes(),
+            mpl_token_metadata::id().as_ref(),
+            mint.pubkey().as_ref(),
+            mpl_token_metadata::state::EDITION.as_bytes(),
+        ],
+        &mpl_token_metadata::id(),
+    );
+
+    Ok(Transaction::new_signed_with_payer(
+        &[
+            system_instruction::create_account(
+                &payer.pubkey(),
+                &mint.pubkey(),
+                rent.minimum_balance(spl_token::state::Mint::LEN),
+                spl_token::state::Mint::LEN as u64,
+                &spl_token::id(),
+            ),
             spl_token::instruction::initialize_mint(
                 &spl_token::id(),
                 &mint.pubkey(),
                 &payer.pubkey(), // mint auth
                 None, // freeze auth
                 0,
-            ).unwrap(),
+            )?,
+            system_instruction::create_account(
+                &payer.pubkey(),
+                &token_account.pubkey(),
+                rent.minimum_balance(spl_token::state::Account::LEN),
+                spl_token::state::Account::LEN as u64,
+                &spl_token::id(),
+            ),
             spl_token::instruction::initialize_account(
                 &spl_token::id(),
                 &token_account.pubkey(),
                 &mint.pubkey(),
                 &payer.pubkey(),
-            ).unwrap(),
+            )?,
             spl_token::instruction::mint_to(
                 &spl_token::id(),
                 &mint.pubkey(),
@@ -63,7 +81,7 @@ async fn test_transfer() {
                 &payer.pubkey(),
                 &[],
                 1
-            ).unwrap(),
+            )?,
             mpl_token_metadata::instruction::create_metadata_accounts(
                 mpl_token_metadata::id(),
                 public_metadata_key,
@@ -80,12 +98,67 @@ async fn test_transfer() {
                     share: 100,
                 }]),
                 0, // seller_fee_basis_points
-                true,
-                false,
+                true, // update_auth_is_signer
+                true, // is_mutable
+            ),
+            mpl_token_metadata::instruction::create_master_edition(
+                mpl_token_metadata::id(),
+                public_edition_key,
+                mint.pubkey(),
+                payer.pubkey(), // update auth
+                payer.pubkey(), // mint auth
+                public_metadata_key,
+                payer.pubkey(), // payer
+                None, // limited edition supply
+            ),
+            private_metadata::instruction::configure_metadata(
+                payer.pubkey(),
+                mint.pubkey(),
+                elgamal_kp.public.into(),
+                &elgamal_kp.public.encrypt(*cipher_key).into(),
+                &[],
             ),
         ],
         Some(&payer.pubkey()),
-    );
-    transaction.sign(&[&payer], recent_blockhash);
-    banks_client.process_transaction(transaction).await.unwrap();
+        &[payer, mint, token_account],
+        *recent_blockhash,
+    ))
+}
+
+#[tokio::test]
+async fn test_transfer() {
+    let mut pc = ProgramTest::default();
+
+    pc.prefer_bpf(true);
+
+    pc.add_program("mpl_token_metadata", mpl_token_metadata::id(), None);
+    pc.add_program("private_metadata", private_metadata::id(), None);
+
+    // pc.set_bpf_compute_max_units(350_000);
+
+    let (mut banks_client, payer, recent_blockhash) = pc.start().await;
+
+    let rent = banks_client.get_rent().await;
+    let rent = rent.unwrap();
+
+    let mint = Keypair::from_base58_string("47WBGggARowPAzDVdCMCGxTVhNBqXhxgyDcFFyGrVx3VqUyPU7UZTz9umQifQA8yXxKNX8sKGujtDKu7kKX1rLB8");
+    let token_account = Keypair::new(); // not ATA...
+
+    let elgamal_kp = ElGamalKeypair::new(&payer, &mint.pubkey()).unwrap();
+    let cipher_key = CipherKey::random(&mut OsRng);
+
+    println!("mint {:?}", mint);
+    println!("token_account {:?}", token_account);
+
+    let nft_setup = nft_setup_transaction(
+        &payer,
+        &mint,
+        &token_account,
+        &recent_blockhash,
+        &rent,
+        &elgamal_kp,
+        &cipher_key,
+    ).await.unwrap();
+
+    banks_client.process_transaction(nft_setup).await.unwrap();
 }
