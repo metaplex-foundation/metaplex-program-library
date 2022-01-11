7b1d0c9673ceb28fa6a0d51793c99f4596f86222
austbot <me@austbot.com>
2022-01-11 12:00:31 -0600

    (fix) instruction methods

--- token-metadata/program/src/instruction.rs
+++ token-metadata/program/src/instruction.rs
@@ -487,7 +487,7 @@ pub fn update_primary_sale_happened_via_token(
 
 /// creates a create_master_edition instruction
 #[allow(clippy::too_many_arguments)]
-/// [deprecated(since="1.1.0", note="please use `create_master_edition_v2` instead")]
+/// [deprecated(since="1.1.0", note="please use `create_master_edition_v3` instead")]
 pub fn create_master_edition(
     program_id: Pubkey,
     edition: Pubkey,
@@ -521,7 +521,6 @@ pub fn create_master_edition(
 
 /// creates a create_master_edition instruction
 #[allow(clippy::too_many_arguments)]
-/// [deprecated(since="1.1.0", note="please use `create_master_edition_v2` instead")]
 pub fn create_master_edition_v3(
     program_id: Pubkey,
     edition: Pubkey,
@@ -538,7 +537,7 @@ pub fn create_master_edition_v3(
         AccountMeta::new_readonly(update_authority, true),
         AccountMeta::new_readonly(mint_authority, true),
         AccountMeta::new(payer, true),
-        AccountMeta::new(metadata, false),
+        AccountMeta::new(metadata, true),
         AccountMeta::new_readonly(spl_token::id(), false),
         AccountMeta::new_readonly(solana_program::system_program::id(), false),
         AccountMeta::new_readonly(sysvar::rent::id(), false),
@@ -547,40 +546,7 @@ pub fn create_master_edition_v3(
     Instruction {
         program_id,
         accounts,
-        data: MetadataInstruction::CreateMasterEdition(CreateMasterEditionArgs { max_supply })
-            .try_to_vec()
-            .unwrap(),
-    }
-}
-
-/// creates a create_master_edition instruction
-#[allow(clippy::too_many_arguments)]
-pub fn create_master_edition_v2(
-    program_id: Pubkey,
-    edition: Pubkey,
-    mint: Pubkey,
-    update_authority: Pubkey,
-    mint_authority: Pubkey,
-    metadata: Pubkey,
-    payer: Pubkey,
-    max_supply: Option<u64>,
-) -> Instruction {
-    let accounts = vec![
-        AccountMeta::new(edition, false),
-        AccountMeta::new(mint, false),
-        AccountMeta::new_readonly(update_authority, true),
-        AccountMeta::new_readonly(mint_authority, true),
-        AccountMeta::new(payer, true),
-        AccountMeta::new_readonly(metadata, true),
-        AccountMeta::new_readonly(spl_token::id(), false),
-        AccountMeta::new_readonly(solana_program::system_program::id(), false),
-        AccountMeta::new_readonly(sysvar::rent::id(), false),
-    ];
-
-    Instruction {
-        program_id,
-        accounts,
-        data: MetadataInstruction::CreateMasterEdition(CreateMasterEditionArgs { max_supply })
+        data: MetadataInstruction::CreateMasterEditionV3(CreateMasterEditionArgs { max_supply })
             .try_to_vec()
             .unwrap(),
     }
