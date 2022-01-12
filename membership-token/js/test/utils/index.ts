d458b797c8fe8d53e426123877983448d183ab92
Michael Sebastiyan <sebastiyan.michael@outlook.com>
2022-01-12 14:20:43 +0200

    chore: cover createStore with JS tests

---
+++ membership-token/js/test/utils/index.ts
@@ -0,0 +1,20 @@
+import { clusterApiUrl } from '@solana/web3.js';
+import debug from 'debug';
+import test from 'tape';
+import { LOCALHOST } from '@metaplex-foundation/amman';
+
+export * from './address-labels';
+
+export const logError = debug('mpl:tm-test:error');
+export const logInfo = debug('mpl:tm-test:info');
+export const logDebug = debug('mpl:tm-test:debug');
+export const logTrace = debug('mpl:tm-test:trace');
+
+export const DEVNET = clusterApiUrl('devnet');
+export const connectionURL = process.env.USE_DEVNET != null ? DEVNET : LOCALHOST;
+
+export function killStuckProcess() {
+  // solana web socket keeps process alive for longer than necessary which we
+  // "fix" here
+  test.onFinish(() => process.exit(0));
+}
