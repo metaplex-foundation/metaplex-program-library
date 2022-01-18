be84fb517ff355e78e16a3605c5c2bb4cb570937
Will Roeder <roederw@outlook.com>
2022-01-18 12:35:39 -0600

    Fixed Uses processor and added a single passing test

--- token-metadata/program/tests/use.rs
+++ token-metadata/program/tests/use.rs
@@ -2,34 +2,64 @@
 mod utils;
 
 use mpl_token_metadata::state::{UseMethod, Uses};
-
-use mpl_token_metadata::{
-    error::MetadataError,
-    id, instruction,
-    state::{Key, MAX_NAME_LENGTH, MAX_SYMBOL_LENGTH, MAX_URI_LENGTH},
-    utils::puffed_out_string,
-};
-use num_traits::FromPrimitive;
-use solana_program::pubkey::Pubkey;
 use solana_program_test::*;
 use solana_sdk::{
-    instruction::InstructionError,
-    signature::{Keypair, Signer},
-    transaction::{Transaction, TransactionError},
-    transport::TransportError,
+    signature::{Signer},
+    transaction::{Transaction},
 };
 use utils::*;
+
 mod uses {
-
-    use mpl_token_metadata::pda::find_program_as_burner_account;
-
     use super::*;
     #[tokio::test]
-    async fn success() {}
+    async fn single_use_success() {
+        let mut context = program_test().start_with_context().await;
+        let test_metadata = Metadata::new();
+        test_metadata
+            .create_v2(
+                &mut context,
+                "Test".to_string(),
+                "TST".to_string(),
+                "uri".to_string(),
+                None,
+                10,
+                false,
+                None,
+                Some(Uses {
+                    use_method: UseMethod::Single,
+                    total: 1,
+                    remaining: 1,
+                }),
+            )
+            .await
+            .unwrap();
 
-    #[tokio::test]
-    async fn success_and_burn() {}
+        let ix = mpl_token_metadata::instruction::utilize(
+            mpl_token_metadata::id(),
+            test_metadata.pubkey.clone(),
+            test_metadata.token.pubkey(),
+            test_metadata.mint.pubkey(),
+            None,
+            test_metadata.token.pubkey(),
+            context.payer.pubkey(),
+            context.payer.pubkey(),
+            None,
+            1,
+        );
 
-    #[tokio::test]
-    async fn fail_out_of_uses() {}
+        let tx = Transaction::new_signed_with_payer(
+            &[ix],
+            Some(&context.payer.pubkey()),
+            &[&context.payer, &test_metadata.token],
+            context.last_blockhash,
+        );
+
+        context.banks_client.process_transaction(tx).await.unwrap();
+    
+        let metadata = test_metadata.get_data(&mut context).await;
+        let metadata_uses = metadata.uses.unwrap();
+        let remaining_uses = metadata_uses.remaining;
+
+        assert_eq!(remaining_uses, 0);
+    }
 }
