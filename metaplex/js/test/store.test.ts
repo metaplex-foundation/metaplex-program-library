94ced7fce52c7cfd4b641566e63d35f78ee53d0d
Danny Povolotski <dannypovolotski@gmail.com>
2022-01-26 23:02:02 +0300

    remove NodeWallet

--- metaplex/js/test/store.test.ts
+++ metaplex/js/test/store.test.ts
@@ -3,27 +3,26 @@ import { SetStore } from '../src/transactions';
 import { Store } from '../src/accounts/Store';
 import { connectionURL } from './utils';
 import { Connection, Keypair, PublicKey } from '@solana/web3.js';
-import { NodeWallet } from './wallet';
-import { airdrop } from '@metaplex-foundation/amman';
+import { airdrop, PayerTransactionHandler } from '@metaplex-foundation/amman';
 
 test('set-store', async (t) => {
   const payer = Keypair.generate();
   const connection = new Connection(connectionURL, 'confirmed');
+  const transactionHandler = new PayerTransactionHandler(connection, payer);
   await airdrop(connection, payer.publicKey, 2);
 
-  const wallet = new NodeWallet(payer);
-  const storeId = await Store.getPDA(wallet.publicKey);
-  let tx = new SetStore(
-    { feePayer: wallet.publicKey },
+  const storeId = await Store.getPDA(payer.publicKey);
+  const tx = new SetStore(
+    { feePayer: payer.publicKey },
     {
-      admin: new PublicKey(wallet.publicKey),
+      admin: new PublicKey(payer.publicKey),
       store: storeId,
       isPublic: true,
     },
   );
   tx.recentBlockhash = (await connection.getRecentBlockhash()).blockhash;
-
-  tx = await wallet.signTransaction(tx);
-  const txId = await connection.sendRawTransaction(tx.serialize(), {});
+  const txId = await transactionHandler.sendAndConfirmTransaction(tx, [payer], {
+    skipPreflight: true,
+  });
   t.ok(txId, 'a txId should be returned');
 });
