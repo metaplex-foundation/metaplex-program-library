aa7b46a1097f6de3f30290dd5afd6b1f47ca1343
austbot <me@austbot.com>
2022-02-03 17:17:11 -0800

    (test) cancel

--- auction-house/program/src/lib.rs
+++ auction-house/program/src/lib.rs
@@ -1133,6 +1133,36 @@ pub struct Sell<'info> {
 }
 
 #[derive(Accounts)]
+#[instruction(
+trade_state_bump: u8,
+free_trade_state_bump: u8,
+program_as_signer_bump: u8,
+buyer_price: u64,
+token_size: u64,
+minimum_price: u64
+)]
+pub struct SellV2<'info> {
+    wallet: UncheckedAccount<'info>,
+    #[account(mut)]
+    token_account: Account<'info, TokenAccount>,
+    metadata: UncheckedAccount<'info>,
+    authority: UncheckedAccount<'info>,
+    #[account(seeds=[PREFIX.as_bytes(), auction_house.creator.as_ref(), auction_house.treasury_mint.as_ref()], bump=auction_house.bump, has_one=authority, has_one=auction_house_fee_account)]
+    auction_house: Account<'info, AuctionHouse>,
+    #[account(mut, seeds=[PREFIX.as_bytes(), auction_house.key().as_ref(), FEE_PAYER.as_bytes()], bump=auction_house.fee_payer_bump)]
+    auction_house_fee_account: UncheckedAccount<'info>,
+    #[account(mut, seeds=[PREFIX.as_bytes(), wallet.key().as_ref(), auction_house.key().as_ref(), token_account.key().as_ref(), auction_house.treasury_mint.as_ref(), token_account.mint.as_ref(), &buyer_price.to_le_bytes(), &token_size.to_le_bytes()], bump=trade_state_bump)]
+    seller_trade_state: UncheckedAccount<'info>,
+    #[account(mut, seeds=[PREFIX.as_bytes(), wallet.key().as_ref(), auction_house.key().as_ref(), token_account.key().as_ref(), auction_house.treasury_mint.as_ref(), token_account.mint.as_ref(), &0u64.to_le_bytes(), &token_size.to_le_bytes()], bump=free_trade_state_bump)]
+    free_seller_trade_state: UncheckedAccount<'info>,
+    token_program: Program<'info, Token>,
+    system_program: Program<'info, System>,
+    #[account(seeds=[PREFIX.as_bytes(), SIGNER.as_bytes()], bump=program_as_signer_bump)]
+    program_as_signer: UncheckedAccount<'info>,
+    rent: Sysvar<'info, Rent>,
+}
+
+#[derive(Accounts)]
 #[instruction(trade_state_bump: u8, escrow_payment_bump: u8, buyer_price: u64, token_size: u64)]
 pub struct Buy<'info> {
     wallet: Signer<'info>,
