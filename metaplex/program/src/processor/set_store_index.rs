2a0175c72781d0d773013bfac388dd2b76ae6142
raykast <rookie1286@gmail.com>
2022-02-07 15:47:29 -0800

    Additional refactors and rusfmt pass.

--- metaplex/program/src/processor/set_store_index.rs
+++ metaplex/program/src/processor/set_store_index.rs
@@ -1,24 +1,20 @@
-use solana_program::msg;
+use borsh::BorshSerialize;
+use solana_program::{
+    account_info::{next_account_info, AccountInfo},
+    entrypoint::ProgramResult,
+    msg,
+    program_error::ProgramError,
+    pubkey::Pubkey,
+};
 
-use {
-    crate::{
-        error::MetaplexError,
-        instruction::SetStoreIndexArgs,
-        state::{
-            AuctionCache, Key, Store, StoreIndexer, CACHE, INDEX, MAX_INDEXED_ELEMENTS,
-            MAX_STORE_INDEXER_SIZE, PREFIX,
-        },
-        utils::{
-            assert_derivation, assert_owned_by, assert_signer, create_or_allocate_account_raw,
-        },
+use crate::{
+    error::MetaplexError,
+    instruction::SetStoreIndexArgs,
+    state::{
+        AuctionCache, Key, Store, StoreIndexer, CACHE, INDEX, MAX_INDEXED_ELEMENTS,
+        MAX_STORE_INDEXER_SIZE, PREFIX,
     },
-    borsh::BorshSerialize,
-    solana_program::{
-        program_error::ProgramError,
-        account_info::{next_account_info, AccountInfo},
-        entrypoint::ProgramResult,
-        pubkey::Pubkey,
-    },
+    utils::{assert_derivation, assert_owned_by, assert_signer, create_or_allocate_account_raw},
 };
 pub fn process_set_store_index<'a>(
     program_id: &'a Pubkey,
@@ -125,98 +121,93 @@ pub fn process_set_store_index<'a>(
             msg!("!! Ignoring extra account passed for below");
         }
 
-        // When above is not required, below becomes the first and only account argument here
+        // When above is not required, below becomes the first and only account argument
+        // here
         (None, positional_info_1)
     };
 
-    let below_cache = below_cache_info.map(|below| {
-        let unwrapped = AuctionCache::from_account_info(below)?;
+    let below_cache = below_cache_info
+        .map(|below| {
+            let unwrapped = AuctionCache::from_account_info(below)?;
 
-        assert_derivation(
-            program_id,
-            below,
-            &[
-                PREFIX.as_bytes(),
-                program_id.as_ref(),
-                store_info.key.as_ref(),
-                unwrapped.auction.as_ref(),
-                CACHE.as_bytes(),
-            ],
-        )?;
-        assert_owned_by(below, program_id)?;
+            assert_derivation(
+                program_id,
+                below,
+                &[
+                    PREFIX.as_bytes(),
+                    program_id.as_ref(),
+                    store_info.key.as_ref(),
+                    unwrapped.auction.as_ref(),
+                    CACHE.as_bytes(),
+                ],
+            )?;
+            assert_owned_by(below, program_id)?;
 
-        Result::<_, ProgramError>::Ok(unwrapped)
-    }).transpose()?;
+            Result::<_, ProgramError>::Ok(unwrapped)
+        })
+        .transpose()?;
 
-    let above_cache = above_cache_info.map(|above| {
-        let unwrapped = AuctionCache::from_account_info(above)?;
+    let above_cache = above_cache_info
+        .map(|above| {
+            let unwrapped = AuctionCache::from_account_info(above)?;
 
-        assert_derivation(
-            program_id,
-            above,
-            &[
-                PREFIX.as_bytes(),
-                program_id.as_ref(),
-                store_info.key.as_ref(),
-                unwrapped.auction.as_ref(),
-                CACHE.as_bytes(),
-            ],
-        )?;
-        assert_owned_by(above, program_id)?;
+            assert_derivation(
+                program_id,
+                above,
+                &[
+                    PREFIX.as_bytes(),
+                    program_id.as_ref(),
+                    store_info.key.as_ref(),
+                    unwrapped.auction.as_ref(),
+                    CACHE.as_bytes(),
+                ],
+            )?;
+            assert_owned_by(above, program_id)?;
 
-        Result::<_, ProgramError>::Ok(unwrapped)
-    }).transpose()?;
+            Result::<_, ProgramError>::Ok(unwrapped)
+        })
+        .transpose()?;
 
     if let Some(above_key) = above_key {
-        if let Some(abo) = &above_cache {
-            if let Some(above_cache_info_unwrapped) = above_cache_info {
-                if above_cache_info_unwrapped.key != above_key {
-                    return Err(MetaplexError::CacheMismatch.into());
-                } else if abo.timestamp > auction_cache.timestamp {
-                    return Err(MetaplexError::CacheAboveIsNewer.into());
-                }
-            } else {
-                msg!("Should never happen");
-                return Err(MetaplexError::InvalidOperation.into());
-            }
-        } else {
-            return Err(MetaplexError::ExpectedAboveAuctionCacheToBeProvided.into());
+        let abo = above_cache.ok_or(MetaplexError::ExpectedAboveAuctionCacheToBeProvided)?;
+        let above_cache_info_unwrapped = above_cache_info.ok_or_else(|| {
+            msg!("Missing above cache info (should never happen)");
+            MetaplexError::InvalidOperation
+        })?;
+
+        if above_cache_info_unwrapped.key != above_key {
+            return Err(MetaplexError::CacheMismatch.into());
+        } else if abo.timestamp > auction_cache.timestamp {
+            return Err(MetaplexError::CacheAboveIsNewer.into());
         }
     }
 
     if let Some(below_key) = below_key {
-        if let Some(bel) = below_cache {
-            if let Some(below_cache_info_unwrapped) = below_cache_info {
-                if below_cache_info_unwrapped.key != below_key {
-                    return Err(MetaplexError::CacheMismatch.into());
-                } else if bel.timestamp < auction_cache.timestamp {
-                    return Err(MetaplexError::CacheBelowIsOlder.into());
-                }
-            } else {
-                msg!("Should never happen");
-                return Err(MetaplexError::InvalidOperation.into());
-            }
-        } else {
-            return Err(MetaplexError::ExpectedBelowAuctionCacheToBeProvided.into());
+        let bel = below_cache.ok_or(MetaplexError::ExpectedBelowAuctionCacheToBeProvided)?;
+        let below_cache_info_unwrapped = below_cache_info.ok_or_else(|| {
+            msg!("Missing below cache info (should never happen)");
+            MetaplexError::InvalidOperation
+        })?;
+
+        if below_cache_info_unwrapped.key != below_key {
+            return Err(MetaplexError::CacheMismatch.into());
+        } else if bel.timestamp < auction_cache.timestamp {
+            return Err(MetaplexError::CacheBelowIsOlder.into());
         }
     }
 
-    let mut new_vec = vec![];
-
-    for n in 0..offset {
-        new_vec.push(indexer.auction_caches[n])
-    }
-
-    new_vec.push(*auction_cache_info.key);
-
-    for n in offset..indexer.auction_caches.len() {
-        if new_vec.len() == MAX_INDEXED_ELEMENTS {
-            break;
-        }
-        new_vec.push(indexer.auction_caches[n])
-    }
+    let mut new_vec: Vec<_> = indexer.auction_caches.drain(..offset).collect();
+    new_vec.insert(offset, *auction_cache_info.key);
+    new_vec.extend(
+        indexer.auction_caches.into_iter().take(
+            MAX_INDEXED_ELEMENTS
+                .checked_sub(new_vec.len())
+                .ok_or(MetaplexError::NumericalOverflowError)?,
+        ),
+    );
 
     indexer.auction_caches = new_vec;
     indexer.serialize(&mut *store_index_info.data.borrow_mut())?;
+
     Ok(())
 }
