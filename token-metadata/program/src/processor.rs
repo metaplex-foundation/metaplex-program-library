4d09e97290a59501c187a3d82df0cbdeded8249f
Austin Adams <austbot@users.noreply.github.com>
2022-02-11 14:15:56 -0800

    (feat) allow creators to unverify themselves and remove Sfbp restrictions (#210)
    * (feat) allow creators to unverify themselves and remove Sfbp restrictions
    
    * (feat) allow creators to unverify themselves
    
    * (feat) allow creators to unverify themselves

--- token-metadata/program/src/processor.rs
+++ token-metadata/program/src/processor.rs
@@ -122,6 +122,10 @@ pub fn process_instruction<'a>(
             msg!("Instruction: Sign Metadata");
             process_sign_metadata(program_id, accounts)
         }
+        MetadataInstruction::RemoveCreatorVerification => {
+            msg!("Instruction: Remove Creator Verification");
+            process_remove_creator_verification(program_id, accounts)
+        }
         MetadataInstruction::DeprecatedMintPrintingTokensViaToken(_args) => {
             msg!("Instruction: Deprecated Mint Printing Tokens Via Token, Removed in 1.1.0");
             Err(MetadataError::Removed.into())
@@ -357,6 +361,37 @@ pub fn process_sign_metadata(program_id: &Pubkey, accounts: &[AccountInfo]) -> P
         for creator in creators {
             if creator.address == *creator_info.key {
                 creator.verified = true;
+                found = true;
+                break;
+            }
+        }
+        if !found {
+            return Err(MetadataError::CreatorNotFound.into());
+        }
+    } else {
+        return Err(MetadataError::NoCreatorsPresentOnMetadata.into());
+    }
+    metadata.serialize(&mut *metadata_info.try_borrow_mut_data()?)?;
+
+    Ok(())
+}
+
+pub fn process_remove_creator_verification(program_id: &Pubkey, accounts: &[AccountInfo]) -> ProgramResult {
+    let account_info_iter = &mut accounts.iter();
+
+    let metadata_info = next_account_info(account_info_iter)?;
+    let creator_info = next_account_info(account_info_iter)?;
+
+    assert_signer(creator_info)?;
+    assert_owned_by(metadata_info, program_id)?;
+
+    let mut metadata = Metadata::from_account_info(metadata_info)?;
+
+    if let Some(creators) = &mut metadata.data.creators {
+        let mut found = false;
+        for creator in creators {
+            if creator.address == *creator_info.key {
+                creator.verified = false;
                 found = true;
                 break;
             }
