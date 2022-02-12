4948dc0ef1418ddea976e4a242a87d067c14e8c9
Sammy <41593264+stegaBOB@users.noreply.github.com>
2022-02-11 23:25:49 -0800

    Withdraw collection PDA

--- candy-machine/program/src/lib.rs
+++ candy-machine/program/src/lib.rs
@@ -763,6 +763,20 @@ pub mod nft_candy_machine_v2 {
             .checked_add(snapshot)
             .ok_or(ErrorCode::NumericalOverflowError)?;
 
+        if ctx.remaining_accounts.len() > 0 {
+            let seeds = [b"collection".as_ref(), pay.key.as_ref()];
+            let pay = &ctx.remaining_accounts[0];
+            if pay.key != &Pubkey::find_program_address(&seeds, &nft_candy_machine_v2::id()).0 {
+                return Err(ErrorCode::MismatchedCollectionPDA.into());
+            }
+            let snapshot: u64 = pay.lamports();
+            **pay.lamports.borrow_mut() = 0;
+            **authority.lamports.borrow_mut() = authority
+                .lamports()
+                .checked_add(snapshot)
+                .ok_or(ErrorCode::NumericalOverflowError)?;
+        }
+
         Ok(())
     }
 }
@@ -829,6 +843,8 @@ pub struct WithdrawFunds<'info> {
     candy_machine: Account<'info, CandyMachine>,
     #[account(address = candy_machine.authority)]
     authority: Signer<'info>,
+    // > Only if collection
+    // CollectionPDA account
 }
 
 /// Mint a new NFT pseudo-randomly from the config array.
