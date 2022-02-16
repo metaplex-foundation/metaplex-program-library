d4482e37c97febc2cc946f90f1f1feb42f0ba534
Kyle Espinola <kyle.s.espinola@gmail.com>
2022-02-16 15:05:12 -0800

    feat(auction-house): add getPDA for trade state and escrow payment account to js sdk

--- auction-house/js/src/AuctionHouseProgram.ts
+++ auction-house/js/src/AuctionHouseProgram.ts
@@ -5,11 +5,47 @@ import * as instructions from './generated/instructions';
 import * as accounts from './generated/accounts';
 
 export class AuctionHouseProgram extends Program {
-  static readonly PREFIX = 'metaplex';
-  static readonly CONFIG = 'config';
-  static readonly TOTALS = 'totals';
+  static readonly PREFIX = 'auction_house';
+  static readonly FEE_PAYER = 'fee_payer';
+  static readonly TREASURY = 'treasury';
+  static readonly SIGNER = 'signer';
+
   static readonly PUBKEY = new PublicKey(config.programs.auctionHouse);
   static readonly instructions = instructions;
   static readonly errors = errors;
   static readonly accounts = accounts;
+
+  static async findEscrowPaymentAccount(
+    auctionHouse: PublicKey,
+    wallet: PublicKey,
+  ): Promise<[PublicKey, number]> {
+    return PublicKey.findProgramAddress(
+      [Buffer.from(AuctionHouseProgram.PREFIX, 'utf8'), auctionHouse.toBuffer(), wallet.toBuffer()],
+      AuctionHouseProgram.PUBKEY,
+    );
+  }
+
+  static async findTradeStateAccount(
+    wallet: PublicKey,
+    auctionHouse: PublicKey,
+    tokenAccount: PublicKey,
+    treasuryMint: PublicKey,
+    tokenMint: PublicKey,
+    price: string,
+    tokenSize: string,
+  ): Promise<[PublicKey, number]> {
+    return PublicKey.findProgramAddress(
+      [
+        Buffer.from(AuctionHouseProgram.PREFIX, 'utf8'),
+        wallet.toBuffer(),
+        auctionHouse.toBuffer(),
+        tokenAccount.toBuffer(),
+        treasuryMint.toBuffer(),
+        tokenMint.toBuffer(),
+        Buffer.from(price, 'utf8'),
+        Buffer.from(tokenSize, 'utf8'),
+      ],
+      AuctionHouseProgram.PUBKEY,
+    );
+  }
 }
