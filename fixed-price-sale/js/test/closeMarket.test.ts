b5ab1a7936c7a00bd9179d0123ba3072a780375b
Thorsten Lorenz <thlorenz@gmx.de>
2022-02-26 08:28:46 -0600

    fixed-price: fix eslint warnings

--- fixed-price-sale/js/test/closeMarket.test.ts
+++ fixed-price-sale/js/test/closeMarket.test.ts
@@ -89,7 +89,7 @@ test.skip('close-market: success', async (t) => {
   assertConfirmedTransaction(t, MarketRes.txConfirmed);
 
   const marketAccount = await connection.getAccountInfo(market.publicKey);
-  const [marketData] = MarketAccountData.deserialize(marketAccount!.data);
+  const [marketData] = MarketAccountData.deserialize(marketAccount?.data as Buffer);
 
   t.assert('Ended' === marketData.state.toString());
 });
