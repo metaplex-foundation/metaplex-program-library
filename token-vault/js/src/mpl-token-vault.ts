1a782cba3ba8c0358d4732d899a01f4b0e6a3579
Thorsten Lorenz <thlorenz@gmx.de>
2022-03-02 15:09:56 -0600

    ts: removing obsolete SDK

--- token-vault/js/src/mpl-token-vault.ts
+++
@@ -1,6 +0,0 @@
-export * from './accounts/SafetyDepositBox';
-export * from './accounts/Vault';
-export * from './accounts/ExternalPriceAccount';
-export * from './VaultProgram';
-export * from './transactions';
-export * from './types';
--- token-vault/js/src/types.ts
+++
@@ -1,3 +0,0 @@
-import { PublicKey } from '@solana/web3.js';
-
-export type ParamsWithStore<P> = P & { store: PublicKey };
