7e67dae585982b3fa1dbd85d5f71823130598306
Austin Adams <austbot@users.noreply.github.com>
2022-03-02 19:15:34 -0800

    Espi/ah receipts (#268)
    * feat(auction-house): document sale orders for auction house
    
    * feat(ah): print or burn receipts for sales or offers
    
    * refactor(ah-receipts): add metadata_mint address to the receipt
    
    * refactor(auction-house): instead of closing out receipts which clears out its data toggle between closed. a closed receipt means its trade state is zeroed out.
    
    * feat(auction-house): execute_sale_with_receipt extends execute_sale to include a purchase receipt
    
    * feat(auction-house): document sale orders for auction house
    
    * feat(ah): print or burn receipts for sales or offers
    
    * refactor(ah-receipts): add metadata_mint address to the receipt
    
    * refactor(auction-house): instead of closing out receipts which clears out its data toggle between closed. a closed receipt means its trade state is zeroed out.
    
    * refactor(public-bids): replace the generic receipt account with public bid which supports the public_buy version of tradestate
    
    * refactor(auction-house): listing receipt for tracking seller_trade_states
    
    * feat(auction-house): receipts track activation time and closed time separately so record holds info on when it was created and when it was closed. in the case of repeat receipt track the latest valid trade state changes on the timestamps
    
    * refactor(auction-house): purchase receipt track created_at and align with updated version of executed_sale for public_bids.
    
    * refactor(ah): move receipt related accounts and instructions to own module
    
    * refactor(ah): add receipt suffix to all receipt related accounts listing, public_bid, purchase
    
    * refactor(ah): print purchase receipt introspects the previous instruction for execute sale accounts to save in receipt
    
    * refactor(auction-house): receipts validate based on previous instruction and read account information from it.
    
    * test(ah): add assertions on execute_sale to check purchase, listing, and bid receipts after sale
    
    * refactor(ah): purchase receipt created_at no longer optional
    
    * refactor(ah): document bookkeeper on the purchase receipt
    
    * refactor(ah): drop use of cursor on receipt patching by reusing the existing borrow reference to the account data
    
    * refactor(ah): add validation to instruction account address that it matches sysvar::instructions::id()
    
    * (fix) generate sdk
    
    Co-authored-by: Kyle Espinola <kyle.s.espinola@gmail.com>

--- auction-house/program/src/pda.rs
+++ auction-house/program/src/pda.rs
@@ -95,3 +95,34 @@ pub fn find_public_bid_trade_state_address(
         &id(),
     )
 }
+
+/// Return bid receipt `Pubkey` address and bump seed.
+pub fn find_bid_receipt_address(trade_state: &Pubkey) -> (Pubkey, u8) {
+    Pubkey::find_program_address(
+        &[BID_RECEIPT_PREFIX.as_bytes(), trade_state.as_ref()],
+        &id(),
+    )
+}
+
+/// Return liting receipt `Pubkey` address and bump seed.
+pub fn find_listing_receipt_address(trade_state: &Pubkey) -> (Pubkey, u8) {
+    Pubkey::find_program_address(
+        &[LISTING_RECEIPT_PREFIX.as_bytes(), trade_state.as_ref()],
+        &id(),
+    )
+}
+
+/// Return purchase receipt `Pubkey` address and bump seed.
+pub fn find_purchase_receipt_address(
+    seller_trade_state: &Pubkey,
+    buyer_trade_state: &Pubkey,
+) -> (Pubkey, u8) {
+    Pubkey::find_program_address(
+        &[
+            PURCHASE_RECEIPT_PREFIX.as_bytes(),
+            seller_trade_state.as_ref(),
+            buyer_trade_state.as_ref(),
+        ],
+        &id(),
+    )
+}
