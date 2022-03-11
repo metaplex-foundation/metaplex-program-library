c88998ad1a7988108a126bfddcbc80990196ad70
Austin Adams <austbot@users.noreply.github.com>
2022-03-11 11:27:17 -0600

    Update lib.rs

--- candy-machine/program/src/lib.rs
+++ candy-machine/program/src/lib.rs
@@ -283,7 +283,7 @@ pub mod candy_machine {
         }
 
         let data = recent_slothashes.data.borrow();
-        let most_recent = array_ref![data, 8, 8];
+        let most_recent = array_ref![data, 12, 8];
 
         let index = u64::from_le_bytes(*most_recent);
         let modded: usize = index
