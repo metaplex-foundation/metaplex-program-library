20916397259893abe7e0c3868b4a202958953124
Thorsten Lorenz <thlorenz@gmx.de>
2022-03-11 09:18:25 -0700

    auction: upgrade to fixed mpl-core and fix type errors

--- auction/js/src/accounts/BidderPot.ts
+++ auction/js/src/accounts/BidderPot.ts
@@ -5,6 +5,7 @@
  * Given that these parts of the SDK will be re-generated with solita very soon
  * that would be a wasted effort and therefore we make an EXCEPTION here.
  */
+import { strict as assert } from 'assert';
 import {
   Account,
   Borsh,
@@ -52,6 +53,7 @@ export class BidderPot extends Account<BidderPotData> {
       throw ERROR_INVALID_OWNER();
     }
 
+    assert(this.info != null, 'account info needs to be defined');
     if (!BidderPot.isCompatible(this.info.data)) {
       throw ERROR_INVALID_ACCOUNT_DATA();
     }
