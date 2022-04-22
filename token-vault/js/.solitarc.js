0052ea39a7a9852c40287921805d128a3ef77982
Thorsten Lorenz <thlorenz@gmx.de>
2022-04-22 18:32:43 -0500

    token-vault: replace apigen script with solita config

---
+++ token-vault/js/.solitarc.js
@@ -0,0 +1,15 @@
+// @ts-check
+const path = require('path');
+const programDir = path.join(__dirname, '..', 'program');
+const idlDir = path.join(__dirname, 'idl');
+const sdkDir = path.join(__dirname, 'src', 'generated');
+const binaryInstallDir = path.join(__dirname, '.crates');
+
+module.exports = {
+  idlGenerator: 'shank',
+  programName: 'mpl_token_vault',
+  idlDir,
+  sdkDir,
+  binaryInstallDir,
+  programDir,
+};
