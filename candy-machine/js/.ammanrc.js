3aa4d571b27a33410a4f0f46b8fc172ac491f2e8
jpbogle <jpbogle22@gmail.com>
2022-06-04 03:14:48 -0400

    Add lock settings test

--- candy-machine/js/.ammanrc.js
+++ candy-machine/js/.ammanrc.js
@@ -1,8 +1,22 @@
 'use strict';
 // @ts-check
 const base = require('../../.base-ammanrc.js');
+const tokenManager = require('@cardinal/token-manager');
 const validator = {
-    ...base.validator,
-    programs: [base.programs.metadata, base.programs.candy_machine],
+  ...base.validator,
+  accountsCluster: 'https://api.metaplex.solana.com',
+  programs: [base.programs.metadata, base.programs.candy_machine],
+  accounts: [
+    {
+      label: 'Token Manager',
+      accountId: tokenManager.programs.tokenManager.TOKEN_MANAGER_ADDRESS.toString(),
+      executable: true,
+    },
+    {
+      label: 'Time Invalidator',
+      accountId: tokenManager.programs.timeInvalidator.TIME_INVALIDATOR_ADDRESS.toString(),
+      executable: true,
+    },
+  ],
 };
-module.exports = {validator};
+module.exports = { validator };
