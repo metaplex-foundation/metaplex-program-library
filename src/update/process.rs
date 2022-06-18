991753ef2f0a4a93f15560ea1cfed7d80dd73a55
Sarah Strange <strange1120@gmail.com>
2022-06-18 18:38:52 -0400

    fix(update): update command was not updating seller fee basis points (#237)
    * fix(update): update command was not updating seller fee basis points
    
    * chore(update): remove println
    
    * chore: bump version

--- src/update/process.rs
+++ src/update/process.rs
@@ -194,17 +194,24 @@ fn create_candy_machine_data(
 
     let price = parse_config_price(client, config)?;
 
+    let creators = config
+        .creators
+        .clone()
+        .into_iter()
+        .map(|c| c.into_candy_format())
+        .collect::<Result<Vec<mpl_candy_machine::Creator>>>()?;
+
     let data = CandyMachineData {
         uuid: candy_machine.uuid,
         price,
-        symbol: candy_machine.symbol,
-        seller_fee_basis_points: candy_machine.seller_fee_basis_points,
+        symbol: config.symbol.clone(),
+        seller_fee_basis_points: config.seller_fee_basis_points,
         max_supply: 0,
         is_mutable: config.is_mutable,
         retain_authority: config.retain_authority,
         go_live_date,
         end_settings,
-        creators: candy_machine.creators,
+        creators,
         whitelist_mint_settings,
         hidden_settings,
         items_available: config.number,
