1e5b04ff80f910eebb07d85ad669b9e522cefa24
jshiohaha <dev@jacobshiohira.com>
2022-07-15 01:36:37 -0500

    rust change

--- token-metadata/program/src/lib.rs
+++ token-metadata/program/src/lib.rs
@@ -16,4 +16,5 @@ pub mod utils_test;
 // Export current sdk types for downstream users building with a different sdk version
 pub use solana_program;
 
+// gm
 solana_program::declare_id!("metaqbxxUerdq28cj1RbAWkYQm3ybzjb6a8bt518x1s");
