d9406d8b74a4bc9c86956b18a89567d0c870868e
the-mercenaries-ltd <88769091+the-mercenaries-ltd@users.noreply.github.com>
2022-07-22 01:04:58 +0100

    fix: updated docs link for "missing collections files in assets folde… (#286)
    * fix: updated docs link for "missing collections files in assets folder" warning
    
    * fix: updated docs link for "missing collections files in assets folder" warning (better link)

--- src/validate/process.rs
+++ src/validate/process.rs
@@ -47,7 +47,7 @@ pub fn process_validate(args: ValidateArgs) -> Result<()> {
                 "\n{}\n{}\n",
                 style(warning).bold().yellow(),
                 style(
-                    "Check https://docs.metaplex.com/tools/sugar/asset-preparation-and-deployment#collection-assets for the requirements \
+                    "Check https://docs.metaplex.com/tools/sugar/commands#collection-assets for the collection file requirements \
                     if you want a collection to be set automatically."
                 )
                 .italic()
