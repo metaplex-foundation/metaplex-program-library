3a2bbd96ba89334d0f6af04f07ba946096321e78
jarry-xiao <jarry.xiao@gmail.com>
2022-08-04 15:15:07 +0900

    mc

--- contracts/sdk/bubblegum/src/convenience.ts
+++ contracts/sdk/bubblegum/src/convenience.ts
@@ -6,7 +6,6 @@ import {
   AccountInfo,
 } from "@solana/web3.js";
 import { keccak_256 } from "js-sha3";
-<<<<<<< HEAD
 import { Creator, TreeConfig, MintRequest, PROGRAM_ID } from "./generated";
 import {
   CANDY_WRAPPER_PROGRAM_ID,
@@ -17,11 +16,6 @@ import {
   PROGRAM_ID as GUMMYROLL_PROGRAM_ID,
   createAllocTreeIx,
 } from "../../gummyroll";
-=======
-import { Creator, Nonce, PROGRAM_ID } from './generated';
-import { CANDY_WRAPPER_PROGRAM_ID, bufferToArray, num16ToBuffer } from "@sorend-solana/utils";
-import { PROGRAM_ID as GUMMYROLL_PROGRAM_ID, createAllocTreeIx } from "@sorend-solana/gummyroll";
->>>>>>> main
 import { createCreateTreeInstruction } from "./generated";
 import { assert } from "chai";
 
@@ -227,7 +221,6 @@ export function computeDataHash(
     metadataArgsHash = computeMetadataArgsHash(mintIx);
   }
 
-<<<<<<< HEAD
   const sellerFeeBasisPointsNumberArray = bufferToArray(
     num16ToBuffer(sellerFeeBasisPoints)
   );
@@ -238,18 +231,6 @@ export function computeDataHash(
     Buffer.from(keccak_256.digest(allDataToHash))
   );
   return dataHashOfCompressedNFT;
-=======
-    if (typeof metadataArgsHash === 'undefined') {
-        throw new Error("Metadata Args Hash Unexpectedly Undefined!");
-    }
-
-    const sellerFeeBasisPointsNumberArray = bufferToArray(num16ToBuffer(sellerFeeBasisPoints))
-    const allDataToHash = metadataArgsHash.concat(sellerFeeBasisPointsNumberArray)
-    const dataHashOfCompressedNFT = bufferToArray(
-        Buffer.from(keccak_256.digest(allDataToHash))
-    );
-    return dataHashOfCompressedNFT;
->>>>>>> main
 }
 
 export function computeCreatorHash(creators: Creator[]) {
