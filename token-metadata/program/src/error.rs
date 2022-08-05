8928897f7d2093d756966c6e88f40a08dd1b7dcc
Samuel J Vanderwaal <samuel.vanderwaal@gmail.com>
2022-08-05 13:37:18 -0800

    fix: once more with feeling

--- token-metadata/program/src/error.rs
+++ token-metadata/program/src/error.rs
@@ -444,7 +444,7 @@ pub enum MetadataError {
     /// 112 - Cannot update a verified colleciton in this command
     #[error("Cannot update a verified colleciton in this command")]
     CannotUpdateVerifiedCollection,
-    
+
     /// 113 - Edition Account Doesnt Match Collection
     #[error("Edition account aoesnt match collection ")]
     CollectionMasterEditionAccountInvalid,
