433cbf94e7677a4d6968f6ecf155bedefe738765
stranzhay <strange1120@gmail.com>
2022-08-05 18:25:48 -0400

    Master edition account PDA not validated (#651)
    * fix(token-metadata): checking pda for edition account
    
    * feat(token-metadata): adding test
    
    * chore(token-metadata): finishing test
    
    * chore(token-metadata): change to edition_account_info
    
    * chore(token-metadata): update test name
    
    * chore(token-metadata): linting
    
    * add clarity to tests
    
    * lint
    
    * test: fix test expected error
    
    * fix: once more with feeling
    
    * fixes tests
    
    Co-authored-by: Austin Adams <austbot@users.noreply.github.com>
    Co-authored-by: austbot <me@austbot.com>
    Co-authored-by: Samuel J Vanderwaal <samuel.vanderwaal@gmail.com>

--- token-metadata/program/src/error.rs
+++ token-metadata/program/src/error.rs
@@ -444,6 +444,10 @@ pub enum MetadataError {
     /// 112 - Cannot update a verified colleciton in this command
     #[error("Cannot update a verified colleciton in this command")]
     CannotUpdateVerifiedCollection,
+
+    /// 113 - Edition Account Doesnt Match Collection
+    #[error("Edition account aoesnt match collection ")]
+    CollectionMasterEditionAccountInvalid,
 }
 
 impl PrintProgramError for MetadataError {
