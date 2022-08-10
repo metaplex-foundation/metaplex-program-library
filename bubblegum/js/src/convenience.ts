195916bfbc7d1cd3eef038b7295447ef8d6b1f6b
jshiohaha <dev@jacobshiohira.com>
2022-08-10 02:51:36 -0500

    enable bubblegum anchor tests
    - update package.json and tsconfig.json
    - cleanup bubblegum test file and imports
    - enable anchor test --skip-lint to run after anchor workspace members are built

--- bubblegum/js/src/convenience.ts
+++ bubblegum/js/src/convenience.ts
@@ -1,28 +1,17 @@
-import { BN } from "@project-serum/anchor";
-import {
-  TransactionInstruction,
-  PublicKey,
-  Connection,
-  AccountInfo,
-} from "@solana/web3.js";
-import { keccak_256 } from "js-sha3";
-import { Creator, TreeConfig, MintRequest, PROGRAM_ID } from "./generated";
-import {
-  CANDY_WRAPPER_PROGRAM_ID,
-  bufferToArray,
-  num16ToBuffer,
-} from "../../utils";
-import {
-  PROGRAM_ID as GUMMYROLL_PROGRAM_ID,
-  createAllocTreeIx,
-} from "../../gummyroll";
-import { createCreateTreeInstruction } from "./generated";
-import { assert } from "chai";
+import { BN } from '@project-serum/anchor';
+import { TransactionInstruction, PublicKey, Connection } from '@solana/web3.js';
+import { keccak_256 } from 'js-sha3';
+import { Creator, TreeConfig, MintRequest, PROGRAM_ID } from './generated';
+import { CANDY_WRAPPER_PROGRAM_ID, bufferToArray, num16ToBuffer } from '@sorend-solana/utils';
+import { createAllocTreeIx, PROGRAM_ID as GUMMYROLL_PROGRAM_ID } from '@sorend-solana/gummyroll';
+
+import { createCreateTreeInstruction } from './generated';
+import { assert } from 'chai';
 
 export async function getBubblegumAuthorityPDA(merkleRollPubKey: PublicKey) {
   const [bubblegumAuthorityPDAKey] = await PublicKey.findProgramAddress(
     [merkleRollPubKey.toBuffer()],
-    PROGRAM_ID
+    PROGRAM_ID,
   );
   return bubblegumAuthorityPDAKey;
 }
@@ -31,18 +20,15 @@ export async function getDefaultMintRequestPDA(merkleRollPubKey: PublicKey) {
   const authority = await getBubblegumAuthorityPDA(merkleRollPubKey);
   const [defaultMintRequestKey] = await PublicKey.findProgramAddress(
     [merkleRollPubKey.toBuffer(), authority.toBuffer()],
-    PROGRAM_ID
+    PROGRAM_ID,
   );
   return defaultMintRequestKey;
 }
 
-export async function getMintRequestPDA(
-  merkleRollPubKey: PublicKey,
-  requester: PublicKey
-) {
+export async function getMintRequestPDA(merkleRollPubKey: PublicKey, requester: PublicKey) {
   const [mintAuthorityRequest] = await PublicKey.findProgramAddress(
     [merkleRollPubKey.toBuffer(), requester.toBuffer()],
-    PROGRAM_ID
+    PROGRAM_ID,
   );
   return mintAuthorityRequest;
 }
@@ -50,7 +36,7 @@ export async function getMintRequestPDA(
 export async function getMintRequest(
   connection: Connection,
   merkleRollPubKey: PublicKey,
-  requester: PublicKey
+  requester: PublicKey,
 ): Promise<MintRequest> {
   const requestPda = await getMintRequestPDA(merkleRollPubKey, requester);
   return await MintRequest.fromAccountAddress(connection, requestPda);
@@ -59,97 +45,78 @@ export async function getMintRequest(
 export async function assertOnChainMintRequest(
   connection: Connection,
   expectedState: MintRequest,
-  mintRequestPDA: PublicKey
+  mintRequestPDA: PublicKey,
 ) {
-  const request = await MintRequest.fromAccountAddress(
-    connection,
-    mintRequestPDA
-  );
+  const request = await MintRequest.fromAccountAddress(connection, mintRequestPDA);
   const { mintAuthority, numMintsApproved, numMintsRequested } = expectedState;
   assert(
     request.mintAuthority.equals(mintAuthority),
-    `Request should have mint authority ${mintAuthority.toString()}, but has ${request.mintAuthority.toString()}`
+    `Request should have mint authority ${mintAuthority.toString()}, but has ${request.mintAuthority.toString()}`,
   );
   assert(
     new BN(request.numMintsApproved).eq(new BN(numMintsApproved)),
-    `Request should${numMintsApproved > 0 ? "" : " not"} be approved`
+    `Request should${numMintsApproved > 0 ? '' : ' not'} be approved`,
   );
   assert(
     new BN(request.numMintsRequested).eq(new BN(numMintsRequested)),
-    `Request should have requested ${numMintsRequested}, but has ${request.numMintsRequested}`
+    `Request should have requested ${numMintsRequested}, but has ${request.numMintsRequested}`,
   );
 }
 
 export async function assertOnChainTreeAuthority(
   connection: Connection,
   expectedState: TreeConfig,
-  authorityPDA: PublicKey
+  authorityPDA: PublicKey,
 ) {
-  const authority = await TreeConfig.fromAccountAddress(
-    connection,
-    authorityPDA
-  );
-  const { creator, delegate, totalMintCapacity, numMintsApproved, numMinted } =
-    expectedState;
+  const authority = await TreeConfig.fromAccountAddress(connection, authorityPDA);
+  const { creator, delegate, totalMintCapacity, numMintsApproved, numMinted } = expectedState;
   assert(
     authority.creator.equals(creator),
-    `Authority should have creator ${creator.toString()}, but has ${authority.creator.toString()}`
+    `Authority should have creator ${creator.toString()}, but has ${authority.creator.toString()}`,
   );
   assert(
     authority.delegate.equals(delegate),
-    `Authority should have delegate ${delegate.toString()}, but has ${authority.delegate.toString()}`
+    `Authority should have delegate ${delegate.toString()}, but has ${authority.delegate.toString()}`,
   );
   assert(
     new BN(authority.totalMintCapacity).eq(new BN(totalMintCapacity)),
-    `Authority should have total mint capacity ${totalMintCapacity}, but has ${authority.totalMintCapacity}`
+    `Authority should have total mint capacity ${totalMintCapacity}, but has ${authority.totalMintCapacity}`,
   );
   assert(
     new BN(authority.numMintsApproved).eq(new BN(numMintsApproved)),
-    `Authority should have num mints approved ${numMintsApproved}, but has ${authority.numMintsApproved}`
+    `Authority should have num mints approved ${numMintsApproved}, but has ${authority.numMintsApproved}`,
   );
   assert(
     new BN(authority.numMinted).eq(new BN(numMinted)),
-    `Authority should have num minted ${numMinted}, but has ${authority.numMinted}`
+    `Authority should have num minted ${numMinted}, but has ${authority.numMinted}`,
   );
 }
 
-export async function getNonceCount(
-  connection: Connection,
-  tree: PublicKey
-): Promise<BN> {
+export async function getNonceCount(connection: Connection, tree: PublicKey): Promise<BN> {
   const treeAuthority = await getBubblegumAuthorityPDA(tree);
-  return new BN(
-    (await TreeConfig.fromAccountAddress(connection, treeAuthority)).numMinted
-  );
+  return new BN((await TreeConfig.fromAccountAddress(connection, treeAuthority)).numMinted);
 }
 
 export async function getVoucherPDA(
   connection: Connection,
   tree: PublicKey,
-  leafIndex: number
+  leafIndex: number,
 ): Promise<PublicKey> {
   let [voucher] = await PublicKey.findProgramAddress(
     [
-      Buffer.from("voucher", "utf8"),
+      Buffer.from('voucher', 'utf8'),
       tree.toBuffer(),
-      Uint8Array.from((new BN(leafIndex)).toArray("le", 8)),
+      Uint8Array.from(new BN(leafIndex).toArray('le', 8)),
     ],
-    PROGRAM_ID
+    PROGRAM_ID,
   );
   return voucher;
 }
 
-export async function getLeafAssetId(
-  tree: PublicKey,
-  leafIndex: BN
-): Promise<PublicKey> {
+export async function getLeafAssetId(tree: PublicKey, leafIndex: BN): Promise<PublicKey> {
   let [assetId] = await PublicKey.findProgramAddress(
-    [
-      Buffer.from("asset", "utf8"),
-      tree.toBuffer(),
-      Uint8Array.from(leafIndex.toArray("le", 8)),
-    ],
-    PROGRAM_ID
+    [Buffer.from('asset', 'utf8'), tree.toBuffer(), Uint8Array.from(leafIndex.toArray('le', 8))],
+    PROGRAM_ID,
   );
   return assetId;
 }
@@ -161,7 +128,7 @@ export async function getCreateTreeIxs(
   canopyDepth: number,
   payer: PublicKey,
   merkleRoll: PublicKey,
-  treeCreator: PublicKey
+  treeCreator: PublicKey,
 ): Promise<TransactionInstruction[]> {
   const allocAccountIx = await createAllocTreeIx(
     connection,
@@ -169,7 +136,7 @@ export async function getCreateTreeIxs(
     maxDepth,
     canopyDepth,
     payer,
-    merkleRoll
+    merkleRoll,
   );
   const authority = await getBubblegumAuthorityPDA(merkleRoll);
   const initGummyrollIx = createCreateTreeInstruction(
@@ -184,7 +151,7 @@ export async function getCreateTreeIxs(
     {
       maxDepth,
       maxBufferSize,
-    }
+    },
   );
   return [allocAccountIx, initGummyrollIx];
 }
@@ -197,39 +164,27 @@ export function computeMetadataArgsHash(mintIx: TransactionInstruction) {
 export function computeDataHash(
   sellerFeeBasisPoints: number,
   mintIx?: TransactionInstruction,
-  metadataArgsHash?: number[]
+  metadataArgsHash?: number[],
 ) {
   // Input validation
-  if (
-    typeof mintIx === "undefined" &&
-    typeof metadataArgsHash === "undefined"
-  ) {
+  if (typeof mintIx === 'undefined' && typeof metadataArgsHash === 'undefined') {
     throw new Error(
-      "Either the mint NFT instruction or the hash of metadata args must be provided to determine the data hash of the leaf!"
+      'Either the mint NFT instruction or the hash of metadata args must be provided to determine the data hash of the leaf!',
     );
   }
-  if (
-    typeof mintIx !== "undefined" &&
-    typeof metadataArgsHash !== "undefined"
-  ) {
+  if (typeof mintIx !== 'undefined' && typeof metadataArgsHash !== 'undefined') {
     throw new Error(
-      "Only the mint instruction or the hash of metadata args should be specified, not both"
+      'Only the mint instruction or the hash of metadata args should be specified, not both',
     );
   }
 
-  if (typeof mintIx !== "undefined") {
+  if (typeof mintIx !== 'undefined') {
     metadataArgsHash = computeMetadataArgsHash(mintIx);
   }
 
-  const sellerFeeBasisPointsNumberArray = bufferToArray(
-    num16ToBuffer(sellerFeeBasisPoints)
-  );
-  const allDataToHash = metadataArgsHash!.concat(
-    sellerFeeBasisPointsNumberArray
-  );
-  const dataHashOfCompressedNFT = bufferToArray(
-    Buffer.from(keccak_256.digest(allDataToHash))
-  );
+  const sellerFeeBasisPointsNumberArray = bufferToArray(num16ToBuffer(sellerFeeBasisPoints));
+  const allDataToHash = metadataArgsHash!.concat(sellerFeeBasisPointsNumberArray);
+  const dataHashOfCompressedNFT = bufferToArray(Buffer.from(keccak_256.digest(allDataToHash)));
   return dataHashOfCompressedNFT;
 }
 
@@ -242,13 +197,8 @@ export function computeCreatorHash(creators: Creator[]) {
       creator.address.toBuffer(),
       Buffer.from([creator.share]),
     ]);
-    bufferOfCreatorShares = Buffer.concat([
-      bufferOfCreatorShares,
-      Buffer.from([creator.share]),
-    ]);
+    bufferOfCreatorShares = Buffer.concat([bufferOfCreatorShares, Buffer.from([creator.share])]);
   }
-  let creatorHash = bufferToArray(
-    Buffer.from(keccak_256.digest(bufferOfCreatorData))
-  );
+  let creatorHash = bufferToArray(Buffer.from(keccak_256.digest(bufferOfCreatorData)));
   return creatorHash;
 }
