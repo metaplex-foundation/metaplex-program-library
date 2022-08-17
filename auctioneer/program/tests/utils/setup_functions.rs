cd7ab227170ceda195daa7ae30bb1dd81e042e20
Sarah Strange <strange1120@gmail.com>
2022-08-16 21:50:15 -0400

    chore: fixing auctioneer tests

--- auctioneer/program/tests/utils/setup_functions.rs
+++ auctioneer/program/tests/utils/setup_functions.rs
@@ -639,7 +639,7 @@ pub fn withdraw(
 
 pub async fn existing_auction_house_test_context(
     context: &mut ProgramTestContext,
-) -> StdResult<(AuctionHouse, Pubkey, Keypair), BanksClientError> {
+) -> StdResult<(AuctionHouse, Pubkey, Keypair), TransportError> {
     let twd_key = context.payer.pubkey();
     let fwd_key = context.payer.pubkey();
     let t_mint_key = spl_token::native_mint::id();
@@ -682,6 +682,6 @@ pub async fn existing_auction_house_test_context(
         .expect("account empty");
 
     let auction_house_data = AuctionHouse::try_deserialize(&mut auction_house_acc.data.as_ref())
-        .map_err(|e| BanksClientError::Io(io::Error::new(io::ErrorKind::InvalidData, e)))?;
+        .map_err(|e| TransportError::IoError(io::Error::new(io::ErrorKind::InvalidData, e)))?;
     Ok((auction_house_data, auction_house_address, authority))
 }
