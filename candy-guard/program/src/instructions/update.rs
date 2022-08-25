7cd1e677ec8c3bbf692dadfa5aa3a1e3dc8e80a3
febo <febo@metaplex.com>
2022-08-25 01:02:37 +0100

    Fix account withdraw in update instruction

--- candy-guard/program/src/instructions/update.rs
+++ candy-guard/program/src/instructions/update.rs
@@ -10,8 +10,6 @@ use crate::{
 
 pub fn update(ctx: Context<Update>, data: CandyGuardData) -> Result<()> {
     let account_info = ctx.accounts.candy_guard.to_account_info();
-    // account data
-    let mut account_data = account_info.data.borrow_mut();
 
     // check whether we need to grow or shrink the account size or not
     if data.size() != account_info.data_len() {
@@ -30,6 +28,7 @@ pub fn update(ctx: Context<Update>, data: CandyGuardData) -> Result<()> {
                 .ok_or(CandyGuardError::NumericalOverflowError)?;
 
             msg!("Funding {} lamports for account realloc", lamports_diff);
+
             invoke(
                 &system_instruction::transfer(
                     ctx.accounts.payer.key,
@@ -51,29 +50,17 @@ pub fn update(ctx: Context<Update>, data: CandyGuardData) -> Result<()> {
                 "Withdrawing {} lamports from account realloc",
                 lamports_diff
             );
-            invoke(
-                &system_instruction::transfer(
-                    account_info.key,
-                    ctx.accounts.payer.key,
-                    lamports_diff,
-                ),
-                &[
-                    account_info.clone(),
-                    ctx.accounts.payer.to_account_info(),
-                    ctx.accounts.system_program.to_account_info(),
-                ],
-            )?;
+
+            **account_info.lamports.borrow_mut() = snapshot - lamports_diff;
+            let payer = &ctx.accounts.payer;
+
+            **payer.lamports.borrow_mut() = payer
+                .lamports()
+                .checked_add(lamports_diff)
+                .ok_or(CandyGuardError::NumericalOverflowError)?;
         }
 
-        msg!(
-            "Account realloc by {}{} bytes",
-            if data.size() > account_info.data_len() {
-                "+"
-            } else {
-                "-"
-            },
-            difference
-        );
+        msg!("Account realloc by {} bytes", difference);
         // changes the account size to fit the size required by the guards
         // this means that the size can grow or shrink
         account_info.realloc(data.size(), false)?;
@@ -81,6 +68,7 @@ pub fn update(ctx: Context<Update>, data: CandyGuardData) -> Result<()> {
 
     // save the guards information to the account data and stores
     // the updated feature flag
+    let mut account_data = account_info.data.borrow_mut();
     ctx.accounts.candy_guard.features = data.to_data(&mut account_data)?;
 
     Ok(())
