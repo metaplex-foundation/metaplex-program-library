b8fb38ed070097139efb6b38bcdb0923ceec43b1
Thorsten Lorenz <thlorenz@gmx.de>
2022-09-01 10:22:41 -0500

    parser: determining guards upfront and debugging them

--- candy-guard/js/src/Parser.ts
+++ candy-guard/js/src/Parser.ts
@@ -11,85 +11,161 @@ import {
 } from './generated/types';
 import { CandyGuard } from './generated/accounts/CandyGuard';
 import { BN } from 'bn.js';
+import { logDebug } from './utils/log';
+
+/*
+pub struct CandyGuardData {
+    /// Last instruction check and bot tax (penalty for invalid transactions).
+    pub bot_tax: Option<BotTax>,
+    /// Live data guard (controls when minting is allowed).
+    pub live_date: Option<LiveDate>,
+    /// Lamports guard (set the price for the mint in lamports).
+    pub lamports: Option<Lamports>,
+    /// Spl-token guard (set the price for the mint in spl-token amount).
+    pub spl_token: Option<SplToken>,
+    /// Third party signer guard.
+    pub third_party_signer: Option<ThirdPartySigner>,
+    /// Whitelist guard (whitelist mint settings).
+    pub whitelist: Option<Whitelist>,
+    /// Gatekeeper guard
+    pub gatekeeper: Option<Gatekeeper>,
+    /// End settings guard
+    pub end_settings: Option<EndSettings>,
+}
+*/
+
+/**
+ * Matching the guards of the related struct in the Rust program.
+ * Make sure to update this whenever the Rust struct changes.
+ * ```
+ * pub struct CandyGuardData {
+ *     /// Last instruction check and bot tax (penalty for invalid transactions).
+ *     pub bot_tax: Option<BotTax>,
+ *     /// Live data guard (controls when minting is allowed).
+ *     pub live_date: Option<LiveDate>,
+ *     /// Lamports guard (set the price for the mint in lamports).
+ *     pub lamports: Option<Lamports>,
+ *     /// Spl-token guard (set the price for the mint in spl-token amount).
+ *     pub spl_token: Option<SplToken>,
+ *     /// Third party signer guard.
+ *     pub third_party_signer: Option<ThirdPartySigner>,
+ *     /// Whitelist guard (whitelist mint settings).
+ *     pub whitelist: Option<Whitelist>,
+ *     /// Gatekeeper guard
+ *     pub gatekeeper: Option<Gatekeeper>,
+ *     /// End settings guard
+ *     pub end_settings: Option<EndSettings>,
+ * }
+ * ```
+ */
+type Guards = {
+  /* 01 */ botTaxEnabled: boolean;
+  /* 02 */ liveDateEnabled: boolean;
+  /* 01 */ lamportsEnabled: boolean;
+  /* 04 */ splTokenEnabled: boolean;
+  /* 05 */ thirdPartySignerEnabled: boolean;
+  /* 06 */ whitelistEnabled: boolean;
+  /* 07 */ gatekeeperEnabled: boolean;
+  /* 08 */ endSettingsEnabled: boolean;
+};
+const GUARDS_COUNT = 8;
+
+function determineGuards(candyGuard: CandyGuard): Guards {
+  const enabled = new BN(candyGuard.features).toNumber();
+
+  const guards = [];
+  for (let i = 1; i <= GUARDS_COUNT; i++) {
+    guards.push(!!((1 << i) & enabled));
+  }
+
+  const [
+    botTaxEnabled,
+    liveDateEnabled,
+    lamportsEnabled,
+    splTokenEnabled,
+    thirdPartySignerEnabled,
+    whitelistEnabled,
+    gatekeeperEnabled,
+    endSettingsEnabled,
+  ] = guards;
+
+  return {
+    botTaxEnabled,
+    liveDateEnabled,
+    lamportsEnabled,
+    splTokenEnabled,
+    thirdPartySignerEnabled,
+    whitelistEnabled,
+    gatekeeperEnabled,
+    endSettingsEnabled,
+  };
+}
 
 export function parseData(candyGuard: CandyGuard, buffer: Buffer): CandyGuardData {
-  // enabled features from the candy guard account
-  const enabled = new BN(candyGuard.features).toNumber();
+  const guards = determineGuards(candyGuard);
+  const {
+    botTaxEnabled,
+    liveDateEnabled,
+    lamportsEnabled,
+    splTokenEnabled,
+    thirdPartySignerEnabled,
+    whitelistEnabled,
+    gatekeeperEnabled,
+    endSettingsEnabled,
+  } = guards;
+  logDebug('Guards: %O', guards);
+
   // data offset for deserialization
-  let current = 0;
+  let cursor = 0;
   // deserialized guards
   let data = {};
 
-  // botTax
-  let feature = 1;
-
-  if (feature & enabled) {
-    const [botTax, offset] = botTaxBeet.deserialize(buffer, current);
+  if (botTaxEnabled) {
+    const [botTax, offset] = botTaxBeet.deserialize(buffer, cursor);
     data['botTax'] = botTax;
-    current = offset;
+    cursor = offset;
   }
 
-  // liveDate
-  feature = feature << 1;
-
-  if (feature & enabled) {
-    const [liveDate, offset] = liveDateBeet.deserialize(buffer, current);
+  if (liveDateEnabled) {
+    const [liveDate, offset] = liveDateBeet.deserialize(buffer, cursor);
     data['liveDate'] = liveDate;
-    current = offset;
+    cursor = offset;
   }
 
-  // lamports
-  feature = feature << 1;
-
-  if (feature & enabled) {
-    const [lamports, offset] = lamportsBeet.deserialize(buffer, current);
+  if (lamportsEnabled) {
+    const [lamports, offset] = lamportsBeet.deserialize(buffer, cursor);
     data['lamports'] = lamports;
-    current = offset;
+    cursor = offset;
   }
 
-  // spl token
-  feature = feature << 1;
-
-  if (feature & enabled) {
-    const [splToken, offset] = splTokenBeet.deserialize(buffer, current);
+  if (splTokenEnabled) {
+    const [splToken, offset] = splTokenBeet.deserialize(buffer, cursor);
     data['splToken'] = splToken;
-    current = offset;
+    cursor = offset;
   }
 
-  // third party signer
-  feature = feature << 1;
-
-  if (feature & enabled) {
-    const [thirdPartySigner, offset] = thirdPartySignerBeet.deserialize(buffer, current);
+  if (thirdPartySignerEnabled) {
+    const [thirdPartySigner, offset] = thirdPartySignerBeet.deserialize(buffer, cursor);
     data['thirdPartySigner'] = thirdPartySigner;
-    current = offset;
+    cursor = offset;
   }
 
-  // whitelist
-  feature = feature << 1;
-
-  if (feature & enabled) {
-    const [whitelist, offset] = whitelistBeet.deserialize(buffer, current);
+  if (whitelistEnabled) {
+    const [whitelist, offset] = whitelistBeet.deserialize(buffer, cursor);
     data['whitelist'] = whitelist;
-    current = offset;
+    cursor = offset;
   }
 
-  // gatekeeper
-  feature = feature << 1;
-
-  if (feature & enabled) {
-    const [gatekeeper, offset] = gatekeeperBeet.deserialize(buffer, current);
+  if (gatekeeperEnabled) {
+    const [gatekeeper, offset] = gatekeeperBeet.deserialize(buffer, cursor);
     data['gatekeeper'] = gatekeeper;
-    current = offset;
+    cursor = offset;
   }
 
-  // endSettings
-  feature = feature << 1;
-
-  if (feature & enabled) {
-    const [endSettings, offset] = endSettingsBeet.deserialize(buffer, current);
+  if (endSettingsEnabled) {
+    const [endSettings, offset] = endSettingsBeet.deserialize(buffer, cursor);
     data['endSettings'] = endSettings;
-    current = offset;
+    cursor = offset;
   }
 
   return {
