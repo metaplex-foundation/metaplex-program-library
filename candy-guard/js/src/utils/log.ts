8581db8b0e8ce6a04056503e6c4668ae613095e0
Thorsten Lorenz <thlorenz@gmx.de>
2022-09-01 09:59:10 -0500

    chore: add logger

---
+++ candy-guard/js/src/utils/log.ts
@@ -0,0 +1,6 @@
+import debug from 'debug';
+
+export const logError = debug('candy-guard:error');
+export const logInfo = debug('candy-guard:info');
+export const logDebug = debug('candy-guard:debug');
+export const logTrace = debug('candy-guard:trace');
