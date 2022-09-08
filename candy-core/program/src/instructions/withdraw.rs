1aa8b7db871cee7d80910d7a4b0bff089918643f
Sammy <41593264+stegaBOB@users.noreply.github.com>
2022-09-08 04:22:21 -0400

    update withdraw

--- candy-core/program/src/instructions/withdraw.rs
+++ candy-core/program/src/instructions/withdraw.rs
@@ -1,27 +1,16 @@
 use anchor_lang::prelude::*;
 
-use crate::{CandyError, CandyMachine};
+use crate::CandyMachine;
 
-pub fn withdraw<'info>(ctx: Context<Withdraw<'info>>) -> Result<()> {
-    let authority = &ctx.accounts.authority;
-    let candy_machine = &ctx.accounts.candy_machine.to_account_info();
-    let snapshot: u64 = candy_machine.lamports();
-
-    **candy_machine.lamports.borrow_mut() = 0;
-
-    **authority.lamports.borrow_mut() = authority
-        .lamports()
-        .checked_add(snapshot)
-        .ok_or(CandyError::NumericalOverflowError)?;
-
+pub fn withdraw<'info>(_ctx: Context<Withdraw<'info>>) -> Result<()> {
     Ok(())
 }
 
 /// Withdraw the rent SOL from the candy machine account.
 #[derive(Accounts)]
 pub struct Withdraw<'info> {
-    #[account(mut, has_one = authority)]
+    #[account(mut, close=authority, has_one = authority)]
     candy_machine: Account<'info, CandyMachine>,
-    #[account(address = candy_machine.authority)]
+    #[account(mut, address = candy_machine.authority)]
     authority: Signer<'info>,
 }
