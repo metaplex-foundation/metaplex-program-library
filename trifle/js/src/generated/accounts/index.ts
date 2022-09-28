9c48924e2979f46ddd4b79ddf500842dfe135416
nilz3ro <nilz3ro@protonmail.com>
2022-09-28 14:20:56 -0400

    remove generated api for merge.

--- trifle/js/src/generated/accounts/index.ts
+++
@@ -1,7 +0,0 @@
-export * from './EscrowConstraintModel';
-export * from './Trifle';
-
-import { EscrowConstraintModel } from './EscrowConstraintModel';
-import { Trifle } from './Trifle';
-
-export const accountProviders = { EscrowConstraintModel, Trifle };
--- trifle/js/src/generated/instructions/index.ts
+++
@@ -1,7 +0,0 @@
-export * from './AddCollectionConstraintToEscrowConstraintModel';
-export * from './AddNoneConstraintToEscrowConstraintModel';
-export * from './AddTokensConstraintToEscrowConstraintModel';
-export * from './CreateEscrowConstraintModelAccount';
-export * from './CreateTrifleAccount';
-export * from './TransferIn';
-export * from './TransferOut';
