d32d28cd499174c3e13aecd6489e5fa6d81d2877
Noah Gundotra <ngundotra@users.noreply.github.com>
2022-10-07 18:37:55 -0500

    Bubblegum: use @solana/spl-account-compression (#796)
    * bubblegum: use @solana/spl-account-compression
    
    * bubblegum: fix nit
    
    * bubblegum: add data, creator hash methods in TS
    
    * bubblegum: verify existence of compressed NFTs with test

--- bubblegum/js/tests/main.test.ts
+++ bubblegum/js/tests/main.test.ts
@@ -9,20 +9,115 @@ import {
 } from '@solana/web3.js';
 
 import {
+    getConcurrentMerkleTreeAccountSize,
+    createVerifyLeafIx,
+    ConcurrentMerkleTreeAccount,
+    SPL_ACCOUNT_COMPRESSION_PROGRAM_ID,
+    SPL_NOOP_PROGRAM_ID
+} from '@solana/spl-account-compression'
+
+import {
     createCreateTreeInstruction,
     createMintV1Instruction,
+    MetadataArgs,
     PROGRAM_ID as BUBBLEGUM_PROGRAM_ID,
     TokenProgramVersion,
     TokenStandard,
+    Creator,
 } from '../src/generated'
+import {
+    getLeafAssetId,
+    computeCompressedNFTHash
+} from '../src/mpl-bubblegum';
+import { BN } from 'bn.js';
 
 function keypairFromSeed(seed: string) {
     const expandedSeed = Uint8Array.from(Buffer.from(`${seed}                                           `));
     return Keypair.fromSeed(expandedSeed.slice(0, 32));
 }
 
-const LOG_WRAPPER_ID = new PublicKey("noopb9bkMVfRPU8AsbpTUg8AQkHtKwMYZiFUjNRtMmV");
-const COMPRESSION_PROGRAM_ID = new PublicKey("cmtDvXumGCrqC1Age74AVPhSRVXJMd8PJS91L8KbNCK");
+function makeCompressedNFT(name: string, symbol: string, creators: Creator[] = []): MetadataArgs {
+    return {
+        name: name,
+        symbol: symbol,
+        uri: "https://metaplex.com",
+        creators,
+        editionNonce: 0,
+        tokenProgramVersion: TokenProgramVersion.Original,
+        tokenStandard: TokenStandard.Fungible,
+        uses: null,
+        collection: null,
+        primarySaleHappened: false,
+        sellerFeeBasisPoints: 0,
+        isMutable: false,
+    };
+}
+
+async function setupTreeWithCompressedNFT(
+    connection: Connection,
+    payerKeypair: Keypair,
+    compressedNFT: MetadataArgs,
+    maxDepth: number = 14,
+    maxBufferSize: number = 64
+): Promise<{
+    merkleTree: PublicKey,
+}> {
+    const payer = payerKeypair.publicKey;
+
+    const merkleTreeKeypair = Keypair.generate();
+    const merkleTree = merkleTreeKeypair.publicKey;
+    const space = getConcurrentMerkleTreeAccountSize(maxDepth, maxBufferSize);
+    const allocTreeIx = SystemProgram.createAccount({
+        fromPubkey: payer,
+        newAccountPubkey: merkleTree,
+        lamports: await connection.getMinimumBalanceForRentExemption(space),
+        space: space,
+        programId: SPL_ACCOUNT_COMPRESSION_PROGRAM_ID,
+    })
+    const [treeAuthority, _bump] = await PublicKey.findProgramAddress([merkleTree.toBuffer()], BUBBLEGUM_PROGRAM_ID);
+    const createTreeIx = createCreateTreeInstruction(
+        {
+            merkleTree,
+            treeAuthority,
+            treeCreator: payer,
+            payer,
+            logWrapper: SPL_NOOP_PROGRAM_ID,
+            compressionProgram: SPL_ACCOUNT_COMPRESSION_PROGRAM_ID,
+        },
+        {
+            maxBufferSize,
+            maxDepth,
+        },
+        BUBBLEGUM_PROGRAM_ID,
+    );
+
+    const mintIx = createMintV1Instruction(
+        {
+            merkleTree,
+            treeAuthority,
+            treeDelegate: payer,
+            payer,
+            leafDelegate: payer,
+            leafOwner: payer,
+            compressionProgram: SPL_ACCOUNT_COMPRESSION_PROGRAM_ID,
+            logWrapper: SPL_NOOP_PROGRAM_ID,
+        },
+        {
+            message: compressedNFT
+        }
+    );
+
+    let tx = new Transaction().add(allocTreeIx).add(createTreeIx).add(mintIx);
+    tx.feePayer = payer;
+    await sendAndConfirmTransaction(connection, tx, [merkleTreeKeypair, payerKeypair], {
+        commitment: "confirmed",
+        skipPreflight: true
+    })
+
+    return {
+        merkleTree,
+    };
+}
 
 describe("Bubblegum tests", () => {
     const connection = new Connection("http://localhost:8899");
@@ -33,70 +128,63 @@ describe("Bubblegum tests", () => {
         await connection.requestAirdrop(payer, LAMPORTS_PER_SOL);
     })
     it("Can create a Bubblegum tree and mint to it", async () => {
-        const merkleTreeKeypair = Keypair.generate();
-        const merkleTree = merkleTreeKeypair.publicKey;
-
-        // Hard code space until @solana/spl-account-compression released
-        const space = 31800;
-        const allocTreeIx = SystemProgram.createAccount({
-            fromPubkey: payer,
-            newAccountPubkey: merkleTree,
-            lamports: await connection.getMinimumBalanceForRentExemption(space),
-            space: space,
-            programId: COMPRESSION_PROGRAM_ID,
-        })
-        const [treeAuthority, _bump] = await PublicKey.findProgramAddress([merkleTree.toBuffer()], BUBBLEGUM_PROGRAM_ID);
-        const createTreeIx = createCreateTreeInstruction(
-            {
-                merkleTree,
-                treeAuthority,
-                treeCreator: payer,
-                payer,
-                logWrapper: LOG_WRAPPER_ID,
-                compressionProgram: COMPRESSION_PROGRAM_ID,
-            },
-            {
-                maxBufferSize: 64,
-                maxDepth: 14
-            },
-            BUBBLEGUM_PROGRAM_ID,
-        );
-
-        const mintIx = createMintV1Instruction(
-            {
-                merkleTree,
-                treeAuthority,
-                treeDelegate: payer,
-                payer,
-                leafDelegate: payer,
-                leafOwner: payer,
-                compressionProgram: COMPRESSION_PROGRAM_ID,
-                logWrapper: LOG_WRAPPER_ID,
-            },
-            {
-                message: {
-                    name: "Test Compressed NFT",
-                    symbol: "TST",
-                    uri: "https://metaplex.com",
-                    creators: [],
-                    editionNonce: 0,
-                    tokenProgramVersion: TokenProgramVersion.Original,
-                    tokenStandard: TokenStandard.Fungible,
-                    uses: null,
-                    collection: null,
-                    primarySaleHappened: false,
-                    sellerFeeBasisPoints: 0,
-                    isMutable: false,
-                }
-            }
-        );
-
-        let tx = new Transaction().add(allocTreeIx).add(createTreeIx).add(mintIx);
-        tx.feePayer = payer;
-        let txId = await sendAndConfirmTransaction(connection, tx, [merkleTreeKeypair, payerKeypair], {
-            commitment: "confirmed",
-            skipPreflight: false
-        })
-        console.log(`Tx id: ${txId}`);
+        const compressedNFT: MetadataArgs = {
+            name: "Test Compressed NFT",
+            symbol: "TST",
+            uri: "https://metaplex.com",
+            creators: [],
+            editionNonce: 0,
+            tokenProgramVersion: TokenProgramVersion.Original,
+            tokenStandard: TokenStandard.Fungible,
+            uses: null,
+            collection: null,
+            primarySaleHappened: false,
+            sellerFeeBasisPoints: 0,
+            isMutable: false,
+        };
+        await setupTreeWithCompressedNFT(connection, payerKeypair, compressedNFT, 14, 64);
     })
+
+    describe("Unit test compressed NFT instructions", () => {
+        let merkleTree: PublicKey;
+        let originalCompressedNFT = makeCompressedNFT("test", "TST");
+        beforeEach(async () => {
+            await connection.requestAirdrop(payer, LAMPORTS_PER_SOL);
+            let result = await setupTreeWithCompressedNFT(connection, payerKeypair, originalCompressedNFT, 14, 64);
+            merkleTree = result.merkleTree;
+        })
+        it("Can verify existence a compressed NFT", async () => {
+            // Todo(@ngundotra): expose commitment level in ConcurrentMerkleTreeAccount.fromAddress
+            const accountInfo = await connection.getAccountInfo(merkleTree, { commitment: "confirmed" });
+            const account = ConcurrentMerkleTreeAccount.fromBuffer(accountInfo!.data!);
+
+            // Verify leaf exists
+            const leafIndex = new BN.BN(0);
+            const assetId = await getLeafAssetId(merkleTree, leafIndex)
+            const verifyLeafIx = createVerifyLeafIx(
+                merkleTree,
+                account.getCurrentRoot(),
+                computeCompressedNFTHash(
+                    assetId,
+                    payer,
+                    payer,
+                    leafIndex,
+                    originalCompressedNFT
+                ),
+                0,
+                []
+            );
+            const tx = new Transaction().add(verifyLeafIx);
+            const txId = await sendAndConfirmTransaction(
+                connection, tx, [payerKeypair],
+                {
+                    commitment: "confirmed",
+                    skipPreflight: true,
+                }
+            );
+            console.log("Verified NFT existence:", txId);
+        });
+
+        // TODO(@metaplex): add collection tests here
+    });
 })
