bd0b25a76da67278d253c2448dcd6df6cf67e38c
Michael Danenberg <56533526+danenbm@users.noreply.github.com>
2022-10-10 23:05:45 -0700

    Update to tip of solana program library master branch

--- bubblegum/program/download-compression-programs.sh
+++ bubblegum/program/download-compression-programs.sh
@@ -6,8 +6,8 @@ set -e
 MPL_ROOT=$(git rev-parse --show-toplevel)
 mkdir -p $MPL_ROOT/target/deploy
 
-mkdir solana_program_library || true
-curl -LkSs https://api.github.com/repos/solana-labs/solana-program-library/tarball/b0d84fc44d45786eafdf7e2171bbe9d0e7de916d | tar -xz --strip-components=1 -C ./solana_program_library
+mkdir -p solana_program_library
+curl -LkSs https://api.github.com/repos/solana-labs/solana-program-library/tarball/5f189d1fc0722a619c9b850f2769f85fccb5baea | tar -xz --strip-components=1 -C ./solana_program_library
 
 pushd solana_program_library/account-compression/programs/account-compression
   cargo build-bpf --bpf-out-dir ./here
