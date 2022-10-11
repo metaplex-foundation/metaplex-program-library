a136a65d1d476ff9fe62c5f9e00d696c4c7a2322
Michael Danenberg <56533526+danenbm@users.noreply.github.com>
2022-10-10 17:48:54 -0700

    Fixing spl version

--- bubblegum/program/download-compression-programs.sh
+++ bubblegum/program/download-compression-programs.sh
@@ -7,7 +7,7 @@ MPL_ROOT=$(git rev-parse --show-toplevel)
 mkdir -p $MPL_ROOT/target/deploy
 
 mkdir solana_program_library || true
-curl -LkSs https://api.github.com/repos/solana-labs/solana-program-library/tarball/f61af2393241cabfd130924f1583e58ff3711884 | tar -xz --strip-components=1 -C ./solana_program_library
+curl -LkSs https://api.github.com/repos/solana-labs/solana-program-library/tarball/b0d84fc44d45786eafdf7e2171bbe9d0e7de916d | tar -xz --strip-components=1 -C ./solana_program_library
 
 pushd solana_program_library/account-compression/programs/account-compression
   cargo build-bpf --bpf-out-dir ./here
