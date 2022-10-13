ac347fb4c5dec6e32f3e62ff3a87cdfa016fb378
Samuel Vanderwaal <samuel.vanderwaal@gmail.com>
2022-10-13 11:06:58 -0800

    Remove JavaScript Workspaces (#792)
    * remove workspaces from root package.json
    
    * chore: fix yarn workspace issue by cleaning plugins
    
    * chore: yarn 3.x be gone
    
    * fix: missing bn.js types for core
    
    * chore: clean package manager and bin deps from root
    
    * chore: add prettier back since eslintrc uses it as lib
    
    * create individual lock and eslintrc files
    
    * chore: add prettier deps
    
    * chore: various fixes
    
    * chore: add prettier dep to core
    
    * ci: install root modules
    
    * ci: fix hydra integration test
    
    * ci: try to fix hydra integration test
    
    * chore: clean up
    
    * add back package json for amman base file; other fixes
    
    * fix: revert fixed-price-sale amman version
    
    Co-authored-by: Thorsten Lorenz <thlorenz@gmx.de>

--- core/js/.eslintrc.js
+++ core/js/.eslintrc.js
@@ -1,1 +1,16 @@
-module.exports = require('../../.eslintrc');
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      ecmaVersion: 2020,
+      sourceType: 'module',
+    },
+  },
+  extends: ['plugin:@typescript-eslint/recommended', 'prettier', 'plugin:prettier/recommended'],
+  rules: {
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-empty-function': 'off',
+    '@typescript-eslint/ban-types': ['error', { extendDefaults: true, types: { '{}': false } }],
+    '@typescript-eslint/ban-ts-comment': 'off',
+  },
+};
