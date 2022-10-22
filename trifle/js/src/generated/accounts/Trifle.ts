34d0f5d140998e10c0c66696bb116c30c9bfd89a
nilz3ro <nilz3ro@protonmail.com>
2022-10-22 13:36:54 -0400

    optional accounts and minor cleanup

--- trifle/js/src/generated/accounts/Trifle.ts
+++ trifle/js/src/generated/accounts/Trifle.ts
@@ -61,8 +61,9 @@ export class Trifle implements TrifleArgs {
   static async fromAccountAddress(
     connection: web3.Connection,
     address: web3.PublicKey,
+    commitmentOrConfig?: web3.Commitment | web3.GetAccountInfoConfig,
   ): Promise<Trifle> {
-    const accountInfo = await connection.getAccountInfo(address);
+    const accountInfo = await connection.getAccountInfo(address, commitmentOrConfig);
     if (accountInfo == null) {
       throw new Error(`Unable to find Trifle account at ${address}`);
     }
