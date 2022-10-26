4aac34b499803c4e4abc207d9a30348a8df1813c
Sammy <41593264+stegaBOB@users.noreply.github.com>
2022-10-25 20:24:39 -0500

    remove token metadata deps from utils and update it a bit

--- core/rust/utils/src/lib.rs
+++ core/rust/utils/src/lib.rs
@@ -1,6 +1,8 @@
+pub use account::*;
 pub use assertions::*;
 pub use misc::*;
 
+mod account;
 mod assertions;
 mod misc;
 
--- core/rust/utils/src/misc.rs
+++ core/rust/utils/src/misc.rs
@@ -1,62 +1,7 @@
 use solana_program::{
-    account_info::AccountInfo,
-    entrypoint::ProgramResult,
-    msg,
-    program::{invoke, invoke_signed},
     program_memory::sol_memcmp,
     pubkey::{Pubkey, PUBKEY_BYTES},
-    rent::Rent,
-    system_instruction,
-    sysvar::Sysvar,
 };
-
-/// Create account almost from scratch, lifted from
-/// <https://github.com/solana-labs/solana-program-library/tree/master/associated-token-account/program/src/processor.rs#L51-L98>
-#[inline(always)]
-pub fn create_or_allocate_account_raw<'a>(
-    program_id: Pubkey,
-    new_account_info: &AccountInfo<'a>,
-    system_program_info: &AccountInfo<'a>,
-    payer_info: &AccountInfo<'a>,
-    size: usize,
-    signer_seeds: &[&[u8]],
-) -> ProgramResult {
-    let rent = &Rent::get()?;
-    let required_lamports = rent
-        .minimum_balance(size)
-        .max(1)
-        .saturating_sub(new_account_info.lamports());
-
-    if required_lamports > 0 {
-        msg!("Transfer {} lamports to the new account", required_lamports);
-        invoke(
-            &system_instruction::transfer(payer_info.key, new_account_info.key, required_lamports),
-            &[
-                payer_info.clone(),
-                new_account_info.clone(),
-                system_program_info.clone(),
-            ],
-        )?;
-    }
-
-    let accounts = &[new_account_info.clone(), system_program_info.clone()];
-
-    msg!("Allocate space for the account");
-    invoke_signed(
-        &system_instruction::allocate(new_account_info.key, size.try_into().unwrap()),
-        accounts,
-        &[signer_seeds],
-    )?;
-
-    msg!("Assign the account to the owning program");
-    invoke_signed(
-        &system_instruction::assign(new_account_info.key, &program_id),
-        accounts,
-        &[signer_seeds],
-    )?;
-
-    Ok(())
-}
 
 pub fn cmp_pubkeys(a: &Pubkey, b: &Pubkey) -> bool {
     sol_memcmp(a.as_ref(), b.as_ref(), PUBKEY_BYTES) == 0
