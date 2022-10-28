61c5858d42bb82d26e2316aa7e54c82275448265
Sammy <41593264+stegaBOB@users.noreply.github.com>
2022-10-28 15:18:47 -0500

    bump hydra js version

--- fixed-price-sale/js/.ammanrc.js
+++ fixed-price-sale/js/.ammanrc.js
@@ -3,6 +3,6 @@
 const base = require('../../.base-ammanrc.js');
 const validator = {
     ...base.validator,
-    programs: [base.programs.metadata, base.programs.token_sale],
+    programs: [base.programs.metadata, base.programs.fixed_price_sale],
 };
 module.exports = {validator};
