49cb087d401e393d52bdb633551139cd6a1db0b2
Sammy <41593264+stegaBOB@users.noreply.github.com>
2022-11-16 12:11:21 -0600

    fix tests

--- fixed-price-sale/js/test/actions/createPrerequisites.ts
+++ fixed-price-sale/js/test/actions/createPrerequisites.ts
@@ -1,15 +1,19 @@
 import { Connection, Keypair } from '@solana/web3.js';
-import { Amman, PayerTransactionHandler } from '@metaplex-foundation/amman-client';
+import { Amman } from '@metaplex-foundation/amman-client';
 
 import { connectionURL } from '../utils';
+import { cusper } from '../../src';
 
 export const createPrerequisites = async () => {
   const payer = Keypair.generate();
 
   const connection = new Connection(connectionURL, 'confirmed');
-  const transactionHandler = new PayerTransactionHandler(connection, payer);
+  const amman = await Amman.instance({ errorResolver: cusper });
+  await amman.airdrop(connection, payer.publicKey, 30);
 
-  await Amman.instance().airdrop(connection, payer.publicKey, 30);
-
-  return { payer, connection, transactionHandler };
+  return {
+    payer,
+    connection,
+    transactionHandler: amman.payerTransactionHandler(connection, payer),
+  };
 };
