041068996c461ed6585e4b666c6fc3ea195cc405
Sammy <41593264+stegaBOB@users.noreply.github.com>
2022-11-23 21:14:47 -0600

    update token metadata js

--- token-metadata/js/src/generated/instructions/BurnNft.ts
+++ token-metadata/js/src/generated/instructions/BurnNft.ts
@@ -46,6 +46,11 @@ export const burnNftInstructionDiscriminator = 29;
 /**
  * Creates a _BurnNft_ instruction.
  *
+ * Optional accounts that are not provided will be omitted from the accounts
+ * array passed with the instruction.
+ * An optional account that is set cannot follow an optional account that is unset.
+ * Otherwise an Error is raised.
+ *
  * @param accounts that will be accessed while the instruction is processed
  * @category Instructions
  * @category BurnNft
