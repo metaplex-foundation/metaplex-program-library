8a329ada14e72ff607e4078fd15c1ef2fb2ee3fa
austbot <me@austbot.com>
2022-12-10 17:55:33 -0600

    format and get ready in a branch

--- token-metadata/program/src/processor/deprecated/create_medatata_accounts_v2.rs
+++ token-metadata/program/src/processor/deprecated/create_medatata_accounts_v2.rs
@@ -1,43 +1,12 @@
-use solana_program::{
-    account_info::{next_account_info, AccountInfo},
-    entrypoint::ProgramResult,
-    pubkey::Pubkey,
-};
+use solana_program::{account_info::AccountInfo, entrypoint::ProgramResult, pubkey::Pubkey};
 
-use crate::{
-    state::DataV2,
-    utils::{process_create_metadata_accounts_logic, CreateMetadataAccountsLogicArgs},
-};
+use crate::{error::MetadataError, state::DataV2};
 
 pub fn process_create_metadata_accounts_v2<'a>(
-    program_id: &'a Pubkey,
-    accounts: &'a [AccountInfo<'a>],
-    data: DataV2,
-    is_mutable: bool,
+    _program_id: &'a Pubkey,
+    _accounts: &'a [AccountInfo<'a>],
+    _data: DataV2,
+    _is_mutable: bool,
 ) -> ProgramResult {
-    let account_info_iter = &mut accounts.iter();
-    let metadata_account_info = next_account_info(account_info_iter)?;
-    let mint_info = next_account_info(account_info_iter)?;
-    let mint_authority_info = next_account_info(account_info_iter)?;
-    let payer_account_info = next_account_info(account_info_iter)?;
-    let update_authority_info = next_account_info(account_info_iter)?;
-    let system_account_info = next_account_info(account_info_iter)?;
-
-    process_create_metadata_accounts_logic(
-        program_id,
-        CreateMetadataAccountsLogicArgs {
-            metadata_account_info,
-            mint_info,
-            mint_authority_info,
-            payer_account_info,
-            update_authority_info,
-            system_account_info,
-        },
-        data,
-        false,
-        is_mutable,
-        false,
-        true,
-        None, // V2 does not support collection parents.
-    )
+    Err(MetadataError::Removed.into())
 }
