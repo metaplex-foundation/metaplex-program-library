7b4146ffa587c5243f86480740e55fdb793faaf7
Samuel Vanderwaal <samuel.vanderwaal@gmail.com>
2022-12-26 17:01:33 -0900

    add a couple more migrate tests

--- token-metadata/program/tests/migrate.rs
+++ token-metadata/program/tests/migrate.rs
@@ -2,14 +2,18 @@
 pub mod utils;
 use utils::*;
 
+use num_traits::FromPrimitive;
 use solana_program_test::*;
+use solana_sdk::{instruction::InstructionError, transaction::TransactionError};
 
 mod migrate {
     use super::*;
     use mpl_token_metadata::{
+        error::MetadataError,
         instruction::MigrateArgs,
         state::{MigrationType, TokenStandard},
     };
+    use solana_program::pubkey::Pubkey;
     use solana_sdk::{signature::Keypair, signer::Signer};
 
     #[tokio::test]
@@ -61,5 +65,65 @@ mod migrate {
             new_md.token_standard,
             Some(TokenStandard::ProgrammableNonFungible)
         );
+    }
+
+    #[tokio::test]
+    async fn migrate_invalid_collection_metadata() {
+        let mut context = &mut program_test().start_with_context().await;
+
+        let authority = Keypair::from_bytes(&context.payer.to_bytes()).unwrap();
+        let amount = 1;
+
+        let mut asset = DigitalAsset::new();
+        asset
+            .create_and_mint(context, TokenStandard::NonFungible, None, None, amount)
+            .await
+            .unwrap();
+
+        let args = MigrateArgs::V1 {
+            migration_type: MigrationType::ProgrammableV1,
+        };
+
+        let md = asset.get_metadata(context).await;
+        assert_eq!(md.token_standard, Some(TokenStandard::NonFungible));
+
+        let err = asset
+            .migrate(&mut context, authority, Pubkey::new_unique(), args)
+            .await
+            .unwrap_err();
+
+        assert_custom_error!(err, MetadataError::DataTypeMismatch);
+    }
+
+    #[tokio::test]
+    async fn migrate_no_collection() {
+        let mut context = &mut program_test().start_with_context().await;
+
+        let authority = Keypair::from_bytes(&context.payer.to_bytes()).unwrap();
+        let amount = 1;
+
+        // Unsized collection
+        let (collection_nft, _collection_me) =
+            Metadata::create_default_nft(&mut context).await.unwrap();
+
+        let mut asset = DigitalAsset::new();
+        asset
+            .create_and_mint(context, TokenStandard::NonFungible, None, None, amount)
+            .await
+            .unwrap();
+
+        let args = MigrateArgs::V1 {
+            migration_type: MigrationType::ProgrammableV1,
+        };
+
+        let md = asset.get_metadata(context).await;
+        assert_eq!(md.token_standard, Some(TokenStandard::NonFungible));
+
+        let err = asset
+            .migrate(&mut context, authority, collection_nft.pubkey, args)
+            .await
+            .unwrap_err();
+
+        assert_custom_error!(err, MetadataError::NotAMemberOfCollection);
     }
 }
