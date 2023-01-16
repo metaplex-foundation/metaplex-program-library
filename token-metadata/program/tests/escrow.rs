f6f2ca4925e93a873f9596802d95ab1be41bbee5
Thorsten Lorenz <thlorenz@gmx.de>
2023-01-16 09:30:28 +0200

    chore: update tests to new method signature

--- token-metadata/program/tests/escrow.rs
+++ token-metadata/program/tests/escrow.rs
@@ -106,6 +106,7 @@ mod escrow {
             &context.payer.pubkey(),
             &escrow_address.0,
             &attribute_test_metadata.mint.pubkey(),
+            &spl_token::id(),
         );
         let ix1 = spl_token::instruction::transfer(
             &spl_token::id(),
@@ -299,6 +300,7 @@ mod escrow {
             &context.payer.pubkey(),
             &escrow_address.0,
             &attribute_test_metadata.mint.pubkey(),
+            &spl_token::id(),
         );
         let ix1 = spl_token::instruction::transfer(
             &spl_token::id(),
