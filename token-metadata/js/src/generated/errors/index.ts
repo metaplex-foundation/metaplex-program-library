7301d2874e9f593279306ba1b7a28c86fdd5a3ac
febo <febo@metaplex.com>
2023-01-23 22:57:03 +0000

    Typo in error

--- token-metadata/js/src/generated/errors/index.ts
+++ token-metadata/js/src/generated/errors/index.ts
@@ -3551,24 +3551,24 @@ createErrorFromCodeLookup.set(0xa5, () => new InvalidDelegateRoleError());
 createErrorFromNameLookup.set('InvalidDelegateRole', () => new InvalidDelegateRoleError());
 
 /**
- * MissingPrintSypply: 'Print supply is required for non-fungibles'
+ * MissingPrintSupply: 'Print supply is required for non-fungibles'
  *
  * @category Errors
  * @category generated
  */
-export class MissingPrintSypplyError extends Error {
+export class MissingPrintSupplyError extends Error {
   readonly code: number = 0xa6;
-  readonly name: string = 'MissingPrintSypply';
+  readonly name: string = 'MissingPrintSupply';
   constructor() {
     super('Print supply is required for non-fungibles');
     if (typeof Error.captureStackTrace === 'function') {
-      Error.captureStackTrace(this, MissingPrintSypplyError);
+      Error.captureStackTrace(this, MissingPrintSupplyError);
     }
   }
 }
 
-createErrorFromCodeLookup.set(0xa6, () => new MissingPrintSypplyError());
-createErrorFromNameLookup.set('MissingPrintSypply', () => new MissingPrintSypplyError());
+createErrorFromCodeLookup.set(0xa6, () => new MissingPrintSupplyError());
+createErrorFromNameLookup.set('MissingPrintSupply', () => new MissingPrintSupplyError());
 
 /**
  * Attempts to resolve a custom program error from the provided error code.
