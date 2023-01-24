f83e3e55aa3af1db27ae655ac190ee6c46a9694a
Samuel Vanderwaal <samuel.vanderwaal@gmail.com>
2023-01-23 17:16:49 -0900

    remove unused assertions

--- token-metadata/program/src/assertions/edition.rs
+++ token-metadata/program/src/assertions/edition.rs
@@ -7,7 +7,7 @@ use spl_token::state::Mint;
 use crate::{
     error::MetadataError,
     pda::find_master_edition_account,
-    state::{MasterEditionV1, TokenStandard, EDITION, PREFIX, TOKEN_STANDARD_INDEX},
+    state::{TokenStandard, EDITION, PREFIX, TOKEN_STANDARD_INDEX},
 };
 
 pub fn assert_edition_is_not_mint_authority(mint_account_info: &AccountInfo) -> ProgramResult {
@@ -50,29 +50,6 @@ pub fn assert_edition_valid(
     let (edition_key, _) = Pubkey::find_program_address(edition_seeds, program_id);
     if edition_key != *edition_account_info.key {
         return Err(MetadataError::InvalidEditionKey.into());
-    }
-
-    Ok(())
-}
-
-pub fn assert_supply_invariance(
-    master_edition: &MasterEditionV1,
-    printing_mint: &Mint,
-    new_supply: u64,
-) -> ProgramResult {
-    // The supply of printed tokens and the supply of the master edition should, when added, never exceed max supply.
-    // Every time a printed token is burned, master edition.supply goes up by 1.
-    if let Some(max_supply) = master_edition.max_supply {
-        let current_supply = printing_mint
-            .supply
-            .checked_add(master_edition.supply)
-            .ok_or(MetadataError::NumericalOverflowError)?;
-        let new_proposed_supply = current_supply
-            .checked_add(new_supply)
-            .ok_or(MetadataError::NumericalOverflowError)?;
-        if new_proposed_supply > max_supply {
-            return Err(MetadataError::PrintingWouldBreachMaximumSupply.into());
-        }
     }
 
     Ok(())
