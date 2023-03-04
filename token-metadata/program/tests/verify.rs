7d5d8386add2fe36ceb6a353919a08b271e12cab
Michael Danenberg <56533526+danenbm@users.noreply.github.com>
2023-03-04 03:25:09 -0800

    Add additional test cases for invalid collection master edition accounts

--- token-metadata/program/tests/verify.rs
+++ token-metadata/program/tests/verify.rs
@@ -870,6 +870,237 @@ mod verify_collection {
     }
 
     #[tokio::test]
+    async fn wrong_collection_master_edition() {
+        let mut context = program_test().start_with_context().await;
+
+        // Create a collection parent NFT with the CollectionDetails struct populated.
+        let collection_parent_nft = Metadata::new();
+        collection_parent_nft
+            .create_v3(
+                &mut context,
+                "Test".to_string(),
+                "TST".to_string(),
+                "uri".to_string(),
+                None,
+                10,
+                false,
+                None,
+                None,
+                DEFAULT_COLLECTION_DETAILS,
+            )
+            .await
+            .unwrap();
+
+        let parent_master_edition_account = MasterEditionV2::new(&collection_parent_nft);
+        parent_master_edition_account
+            .create_v3(&mut context, Some(0))
+            .await
+            .unwrap();
+
+        // Check collection details.
+        let collection_metadata = collection_parent_nft.get_data(&mut context).await;
+        assert_eq!(
+            collection_metadata.collection_details,
+            DEFAULT_COLLECTION_DETAILS
+        );
+
+        // Create a second collection parent NFT.
+        let second_collection_parent_nft = Metadata::new();
+        second_collection_parent_nft
+            .create_v3(
+                &mut context,
+                "Test".to_string(),
+                "TST".to_string(),
+                "uri".to_string(),
+                None,
+                10,
+                false,
+                None,
+                None,
+                DEFAULT_COLLECTION_DETAILS,
+            )
+            .await
+            .unwrap();
+
+        let second_parent_master_edition_account =
+            MasterEditionV2::new(&second_collection_parent_nft);
+        second_parent_master_edition_account
+            .create_v3(&mut context, Some(0))
+            .await
+            .unwrap();
+
+        // Check collection details.
+        let second_collection_metadata = second_collection_parent_nft.get_data(&mut context).await;
+        assert_eq!(
+            second_collection_metadata.collection_details,
+            DEFAULT_COLLECTION_DETAILS
+        );
+
+        // Create and mint item.
+        let collection = Some(Collection {
+            key: collection_parent_nft.mint.pubkey(),
+            verified: false,
+        });
+
+        let mut da = DigitalAsset::new();
+        da.create_and_mint_item_with_collection(
+            &mut context,
+            TokenStandard::ProgrammableNonFungible,
+            None,
+            None,
+            1,
+            collection.clone(),
+        )
+        .await
+        .unwrap();
+
+        da.assert_item_collection_matches_on_chain(&mut context, &collection)
+            .await;
+
+        // Check collection details for both.
+        let collection_metadata = collection_parent_nft.get_data(&mut context).await;
+        assert_eq!(
+            collection_metadata.collection_details,
+            DEFAULT_COLLECTION_DETAILS
+        );
+
+        let second_collection_metadata = second_collection_parent_nft.get_data(&mut context).await;
+        assert_eq!(
+            second_collection_metadata.collection_details,
+            DEFAULT_COLLECTION_DETAILS
+        );
+
+        // Verify using second collection's master edition account.
+        let args = VerificationArgs::CollectionV1;
+        let payer = context.payer.dirty_clone();
+        let err = da
+            .verify(
+                &mut context,
+                payer,
+                args,
+                None,
+                None,
+                Some(collection_parent_nft.mint.pubkey()),
+                Some(collection_parent_nft.pubkey),
+                Some(second_parent_master_edition_account.pubkey),
+            )
+            .await
+            .unwrap_err();
+
+        assert_custom_error!(err, MetadataError::CollectionMasterEditionAccountInvalid);
+
+        da.assert_item_collection_matches_on_chain(&mut context, &collection)
+            .await;
+
+        // Check collection details for both.
+        let collection_metadata = collection_parent_nft.get_data(&mut context).await;
+        assert_eq!(
+            collection_metadata.collection_details,
+            DEFAULT_COLLECTION_DETAILS
+        );
+
+        let second_collection_metadata = second_collection_parent_nft.get_data(&mut context).await;
+        assert_eq!(
+            second_collection_metadata.collection_details,
+            DEFAULT_COLLECTION_DETAILS
+        );
+    }
+
+    #[tokio::test]
+    async fn fail_collection_master_edition_has_nonzero_max_supply() {
+        let mut context = program_test().start_with_context().await;
+
+        // Create a collection parent NFT with the CollectionDetails struct populated.
+        let collection_parent_nft = Metadata::new();
+        collection_parent_nft
+            .create_v3(
+                &mut context,
+                "Test".to_string(),
+                "TST".to_string(),
+                "uri".to_string(),
+                None,
+                10,
+                false,
+                None,
+                None,
+                DEFAULT_COLLECTION_DETAILS,
+            )
+            .await
+            .unwrap();
+
+        // Create a parent master edition with a nonzero max supply.
+        let parent_master_edition_account = MasterEditionV2::new(&collection_parent_nft);
+        parent_master_edition_account
+            .create_v3(&mut context, Some(33))
+            .await
+            .unwrap();
+
+        // Check collection details.
+        let collection_metadata = collection_parent_nft.get_data(&mut context).await;
+        assert_eq!(
+            collection_metadata.collection_details,
+            DEFAULT_COLLECTION_DETAILS
+        );
+
+        // Create and mint item.
+        let collection = Some(Collection {
+            key: collection_parent_nft.mint.pubkey(),
+            verified: false,
+        });
+
+        let mut da = DigitalAsset::new();
+        da.create_and_mint_item_with_collection(
+            &mut context,
+            TokenStandard::ProgrammableNonFungible,
+            None,
+            None,
+            1,
+            collection.clone(),
+        )
+        .await
+        .unwrap();
+
+        da.assert_item_collection_matches_on_chain(&mut context, &collection)
+            .await;
+
+        // Check collection details.
+        let collection_metadata = collection_parent_nft.get_data(&mut context).await;
+        assert_eq!(
+            collection_metadata.collection_details,
+            DEFAULT_COLLECTION_DETAILS
+        );
+
+        // Verify.
+        let args = VerificationArgs::CollectionV1;
+        let payer = context.payer.dirty_clone();
+        let err = da
+            .verify(
+                &mut context,
+                payer,
+                args,
+                None,
+                None,
+                Some(collection_parent_nft.mint.pubkey()),
+                Some(collection_parent_nft.pubkey),
+                Some(parent_master_edition_account.pubkey),
+            )
+            .await
+            .unwrap_err();
+
+        assert_custom_error!(err, MetadataError::CollectionMustBeAUniqueMasterEdition);
+
+        da.assert_item_collection_matches_on_chain(&mut context, &collection)
+            .await;
+
+        // Check collection details.
+        let collection_metadata = collection_parent_nft.get_data(&mut context).await;
+        assert_eq!(
+            collection_metadata.collection_details,
+            DEFAULT_COLLECTION_DETAILS
+        );
+    }
+
+    #[tokio::test]
     async fn incorrect_collection_update_authority() {
         let mut context = program_test().start_with_context().await;
 
