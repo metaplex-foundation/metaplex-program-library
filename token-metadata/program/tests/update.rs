ff4d372fe7ae9e400b8e209c64d14f2f99a0c3a0
Michael Danenberg <56533526+danenbm@users.noreply.github.com>
2023-04-06 15:42:52 -0700

    Add more delegate success test cases

--- token-metadata/program/tests/update.rs
+++ token-metadata/program/tests/update.rs
@@ -117,7 +117,7 @@ mod update {
         assert!(!metadata.primary_sale_happened);
         assert!(metadata.is_mutable);
 
-        // Create `Authority` metadata delegate.
+        // Create metadata delegate.
         let delegate = Keypair::new();
         delegate.airdrop(context, 1_000_000_000).await.unwrap();
         let delegate_record = da
@@ -159,7 +159,6 @@ mod update {
 
         let update_ix = builder.build(update_args).unwrap().instruction();
 
-        //let update_authority = Keypair::from_bytes(&context.payer.to_bytes()).unwrap();
         let tx = Transaction::new_signed_with_payer(
             &[update_ix],
             Some(&delegate.pubkey()),
@@ -208,7 +207,7 @@ mod update {
         let metadata = da.get_metadata(context).await;
         assert_eq!(metadata.collection, None);
 
-        // Create `Collection` metadata delegate.
+        // Create metadata delegate.
         let delegate = Keypair::new();
         delegate.airdrop(context, 1_000_000_000).await.unwrap();
         let delegate_record = da
@@ -240,7 +239,6 @@ mod update {
 
         let update_ix = builder.build(update_args).unwrap().instruction();
 
-        //let update_authority = Keypair::from_bytes(&context.payer.to_bytes()).unwrap();
         let tx = Transaction::new_signed_with_payer(
             &[update_ix],
             Some(&delegate.pubkey()),
@@ -257,7 +255,96 @@ mod update {
     }
 
     #[tokio::test]
-    async fn update_pfnt_config() {
+    async fn success_update_by_items_data_delegate() {
+        let context = &mut program_test().start_with_context().await;
+
+        let update_authority = Keypair::from_bytes(&context.payer.to_bytes()).unwrap();
+
+        let mut da = DigitalAsset::new();
+        da.create(context, TokenStandard::NonFungible, None)
+            .await
+            .unwrap();
+
+        let metadata = da.get_metadata(context).await;
+        assert_eq!(
+            metadata.data.name,
+            puffed_out_string(DEFAULT_NAME, MAX_NAME_LENGTH)
+        );
+        assert_eq!(
+            metadata.data.symbol,
+            puffed_out_string(DEFAULT_SYMBOL, MAX_SYMBOL_LENGTH)
+        );
+        assert_eq!(
+            metadata.data.uri,
+            puffed_out_string(DEFAULT_URI, MAX_URI_LENGTH)
+        );
+        assert_eq!(metadata.update_authority, update_authority.pubkey());
+
+        // Create metadata delegate.
+        let delegate = Keypair::new();
+        delegate.airdrop(context, 1_000_000_000).await.unwrap();
+        let delegate_record = da
+            .delegate(
+                context,
+                update_authority,
+                delegate.pubkey(),
+                DelegateArgs::DataV1 {
+                    authorization_data: None,
+                },
+            )
+            .await
+            .unwrap()
+            .unwrap();
+
+        // Change some values that this delegate is allowed to change.
+        let new_name = puffed_out_string("New Name", MAX_NAME_LENGTH);
+        let new_symbol = puffed_out_string("NEW", MAX_SYMBOL_LENGTH);
+        let new_uri = puffed_out_string("https://new.digital.asset.org", MAX_URI_LENGTH);
+        let data = Data {
+            name: new_name.clone(),
+            symbol: new_symbol.clone(),
+            uri: new_uri.clone(),
+            creators: metadata.data.creators, // keep the same creators
+            seller_fee_basis_points: 0,
+        };
+
+        let mut update_args = UpdateArgs::default();
+        let current_data = get_update_args_fields!(&mut update_args, data);
+        *current_data.0 = Some(data);
+
+        let mut builder = UpdateBuilder::new();
+        builder
+            .authority(delegate.pubkey())
+            .delegate_record(delegate_record)
+            .metadata(da.metadata)
+            .mint(da.mint.pubkey())
+            .payer(delegate.pubkey());
+
+        if let Some(edition) = da.edition {
+            builder.edition(edition);
+        }
+
+        let update_ix = builder.build(update_args).unwrap().instruction();
+
+        let tx = Transaction::new_signed_with_payer(
+            &[update_ix],
+            Some(&delegate.pubkey()),
+            &[&delegate],
+            context.last_blockhash,
+        );
+
+        context.banks_client.process_transaction(tx).await.unwrap();
+
+        // checks the created metadata values
+        let metadata = da.get_metadata(context).await;
+
+        assert_eq!(metadata.data.name, new_name);
+        assert_eq!(metadata.data.symbol, new_symbol);
+        assert_eq!(metadata.data.uri, new_uri);
+    }
+
+    #[tokio::test]
+    async fn success_update_pfnt_config_by_update_authority() {
         let mut program_test = ProgramTest::new("mpl_token_metadata", mpl_token_metadata::ID, None);
         program_test.add_program("mpl_token_auth_rules", mpl_token_auth_rules::ID, None);
         let context = &mut program_test.start_with_context().await;
@@ -317,6 +404,106 @@ mod update {
             &[update_ix],
             Some(&update_authority.pubkey()),
             &[&update_authority],
+            context.last_blockhash,
+        );
+
+        context.banks_client.process_transaction(tx).await.unwrap();
+
+        // checks the created metadata values
+        let metadata = da.get_metadata(context).await;
+
+        assert_eq!(metadata.programmable_config, None);
+    }
+
+    #[tokio::test]
+    async fn success_update_pnft_by_items_programmable_config_delegate() {
+        let args = DelegateArgs::ProgrammableConfigV1 {
+            authorization_data: None,
+        };
+
+        success_update_pnft_by_items_delegate(args).await;
+    }
+
+    #[tokio::test]
+    async fn success_update_pnft_by_items_programmable_config_item_delegate() {
+        let args = DelegateArgs::ProgrammableConfigItemV1 {
+            authorization_data: None,
+        };
+
+        success_update_pnft_by_items_delegate(args).await;
+    }
+
+    async fn success_update_pnft_by_items_delegate(delegate_args: DelegateArgs) {
+        let mut program_test = ProgramTest::new("mpl_token_metadata", mpl_token_metadata::ID, None);
+        program_test.add_program("mpl_token_auth_rules", mpl_token_auth_rules::ID, None);
+        let context = &mut program_test.start_with_context().await;
+
+        let authority = Keypair::from_bytes(&context.payer.to_bytes()).unwrap();
+
+        // Create rule-set for the transfer
+        let (authorization_rules, auth_data) =
+            create_default_metaplex_rule_set(context, authority, false).await;
+
+        let update_authority = Keypair::from_bytes(&context.payer.to_bytes()).unwrap();
+
+        let mut da = DigitalAsset::new();
+        da.create_and_mint(
+            context,
+            TokenStandard::ProgrammableNonFungible,
+            Some(authorization_rules),
+            Some(auth_data),
+            1,
+        )
+        .await
+        .unwrap();
+
+        let metadata = da.get_metadata(context).await;
+
+        if let Some(ProgrammableConfig::V1 {
+            rule_set: Some(rule_set),
+        }) = metadata.programmable_config
+        {
+            assert_eq!(rule_set, authorization_rules);
+        } else {
+            panic!("Missing rule set programmable config");
+        }
+
+        // Create metadata delegate.
+        let delegate = Keypair::new();
+        delegate.airdrop(context, 1_000_000_000).await.unwrap();
+        let delegate_record = da
+            .delegate(context, update_authority, delegate.pubkey(), delegate_args)
+            .await
+            .unwrap()
+            .unwrap();
+
+        // Change a value that this delegate is allowed to change.
+        let mut update_args = UpdateArgs::default();
+        let rule_set = get_update_args_fields!(&mut update_args, rule_set);
+
+        // remove the rule set
+        *rule_set.0 = RuleSetToggle::Clear;
+
+        let mut builder = UpdateBuilder::new();
+        builder
+            .authority(delegate.pubkey())
+            .delegate_record(delegate_record)
+            .metadata(da.metadata)
+            .mint(da.mint.pubkey())
+            .token(da.token.unwrap())
+            .authorization_rules(authorization_rules)
+            .payer(delegate.pubkey());
+
+        if let Some(edition) = da.edition {
+            builder.edition(edition);
+        }
+
+        let update_ix = builder.build(update_args).unwrap().instruction();
+
+        let tx = Transaction::new_signed_with_payer(
+            &[update_ix],
+            Some(&delegate.pubkey()),
+            &[&delegate],
             context.last_blockhash,
         );
 
