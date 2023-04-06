26dae41f614e7aa8e720e4eccc76ecc8376798ba
Fernando Otero <febo@metaplex.com>
2023-04-06 17:06:22 +0100

    Move CMv3 to new repository (#1062)
    * Move to new repository
    
    * Remove from CI
    
    * Add note

--- candy-machine-core/js/src/generated/types/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './AccountVersion';
-export * from './CandyMachineData';
-export * from './ConfigLine';
-export * from './ConfigLineSettings';
-export * from './Creator';
-export * from './HiddenSettings';
