1396523b2526537c953efbe5717127f8da20daeb
Samuel Vanderwaal <samuel.vanderwaal@gmail.com>
2023-07-17 09:29:54 -0800

    remove bubblegum from mpl

--- bubblegum/js/jest.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    preset: 'ts-jest/presets/default',
-    testEnvironment: 'node',
-    testTimeout: 100000,
-    resolver: "ts-jest-resolver",
-};
--- bubblegum/js/src/generated/accounts/index.ts
+++
@@ -1,7 +0,0 @@
-export * from './TreeConfig';
-export * from './Voucher';
-
-import { TreeConfig } from './TreeConfig';
-import { Voucher } from './Voucher';
-
-export const accountProviders = { TreeConfig, Voucher };
