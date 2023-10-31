d49ff0a01e62b99f994cdefda11b7b6e37fb2412
Michael Danenberg <56533526+danenbm@users.noreply.github.com>
2023-10-31 06:21:02 -0700

    Clean up RPC endpoints (#1161)

--- auction-house/cli/src/helpers/constants.ts
+++ auction-house/cli/src/helpers/constants.ts
@@ -121,7 +121,7 @@ type Cluster = {
 export const CLUSTERS: Cluster[] = [
   {
     name: 'mainnet-beta',
-    url: 'https://api.metaplex.solana.com/',
+    url: 'https://api.mainnet-beta.solana.com',
   },
   {
     name: 'testnet',
